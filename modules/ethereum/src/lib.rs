a7c1986f4aa67403720652cfe90a6afd1239bafa
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-04-03 17:14:23 +0300

    Update modules/ethereum/src/lib.rs
    Co-Authored-By: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- modules/ethereum/src/lib.rs
+++ modules/ethereum/src/lib.rs
@@ -125,7 +125,7 @@ pub struct ValidatorsSet {
 pub struct ScheduledChange {
 	/// Validators of this set.
 	pub validators: Vec<Address>,
-	/// Hash of the block which has emitted previous validators
+	/// Hash of the block which has emitted previous validators change signal.
 	/// change signal.
 	pub prev_signal_block: Option<H256>,
 }
