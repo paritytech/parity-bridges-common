1d872cacf05ed844d1d9017daa0e035abed13ba1
Hernando Castano <castano.ha@gmail.com>
2020-05-20 17:19:38 -0400

    Silence dead code warnings

--- relays/ethereum/src/rpc_errors.rs
+++ relays/ethereum/src/rpc_errors.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
+#![allow(dead_code)]
+
 use jsonrpsee::raw::client::RawClientError;
 use jsonrpsee::transport::http::RequestError;
 use serde_json;
