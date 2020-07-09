df7aeef9b8765a72a6c5c0d48d137b659153b226
Tomasz Drwięga <tomasz@parity.io>
2020-07-09 16:22:37 +0200

    Fix remaining issues.

--- deployments/testpoa/update.sh
+++ deployments/testpoa/update.sh
@@ -6,5 +6,14 @@ set -xeu
 
 git pull
 docker-compose build
+# Stop the proxy cause otherwise the network can't be stopped
+cd ./proxy
 docker-compose down
+cd -
+# Restart the network
+docker-compose down
+docker-compose up -d
+
+# Restart the proxy
+cd ./proxy
 docker-compose up -d
