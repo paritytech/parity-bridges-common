483737792268af72e043da386c1bf612058ea4f6
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-07-20 12:12:07 +0300

    auto relay exchange transactions

--- relays/ethereum/src/sync_loop.rs
+++ relays/ethereum/src/sync_loop.rs
@@ -18,6 +18,7 @@ use crate::sync::HeadersSyncParams;
 use crate::sync_types::{
 	HeaderId, HeaderStatus, HeadersSyncPipeline, MaybeConnectionError, QueuedHeader, SubmittedHeaders,
 };
+use crate::utils::retry_backoff;
 
 use async_trait::async_trait;
 use backoff::{backoff::Backoff, ExponentialBackoff};
@@ -46,9 +47,6 @@ const BACKUP_STALL_SYNC_TIMEOUT: Duration = Duration::from_secs(10 * 60);
 /// Delay after connection-related error happened before we'll try
 /// reconnection again.
 const CONNECTION_ERROR_DELAY: Duration = Duration::from_secs(10);
-/// Max delay after connection-unrelated error happened before we'll try the
-/// same request again.
-const MAX_BACKOFF_INTERVAL: Duration = Duration::from_secs(60);
 
 /// Source client trait.
 #[async_trait]
@@ -546,15 +544,6 @@ fn interval(timeout: Duration) -> impl futures::Stream<Item = ()> {
 		async_std::task::sleep(timeout).await;
 		Some(((), ()))
 	})
-}
-
-/// Exponential backoff for connection-unrelated errors retries.
-pub(crate) fn retry_backoff() -> ExponentialBackoff {
-	let mut backoff = ExponentialBackoff::default();
-	// we do not want relayer to stop
-	backoff.max_elapsed_time = None;
-	backoff.max_interval = MAX_BACKOFF_INTERVAL;
-	backoff
 }
 
 /// Process result of the future from a client.
