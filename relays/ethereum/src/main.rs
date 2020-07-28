5a015f59fc3e98992b38999659b29d521a893cd7
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-07-28 15:12:08 +0300

    fix: U256::parse() expects hex string :/

--- relays/ethereum/src/main.rs
+++ relays/ethereum/src/main.rs
@@ -276,8 +276,7 @@ fn ethereum_exchange_submit_params(
 
 	if let Some(eth_nonce) = matches.value_of("eth-nonce") {
 		params.eth_nonce = Some(
-			eth_nonce
-				.parse()
+			ethereum_types::U256::from_dec_str(&eth_nonce)
 				.map_err(|e| format!("Failed to parse eth-nonce: {}", e))?,
 		);
 	}
