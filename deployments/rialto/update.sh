f2ffa62d63df60d958d7fba280d24bdcf7930bd4
Hernando Castano <castano.ha@gmail.com>
2020-08-14 15:39:50 -0400

    Support updates via Git or Docker in update script

--- deployments/rialto/update.sh
+++ deployments/rialto/update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Script used to periodically update the network.
+#
+# If used with no flags then it updates the network using Docker
+# images from the Docker Hub. If used with the `-g` a.k.a the Git
+# flag then it'll build the Docker containers from GitHub.
 
 set -xeu
 
-git pull
+git_flag=false
 
 # Update *_BRIDGE_HASH in .env
 function update_hash {
@@ -12,13 +16,25 @@ function update_hash {
 	echo "$1=$(git rev-parse HEAD)" >> .env
 }
 
-case "${1:-all}" in
-	all) update_hash "NODE_BRIDGE_HASH"; update_hash "ETH_BRIDGE_HASH"; update_hash "RELAY_BRIDGE_HASH";;
-	node) update_hash "NODE_BRIDGE_HASH";;
-	relay) update_hash "RELAY_BRIDGE_HASH";;
-	eth) update_hash "ETH_BRIDGE_HASH";;
-	*) echo "Invalid parameter: $1 (expected all/node/relay/eth)"; exit 1;;
-esac
+while getopts 'g:' flag; do
+  case "${flag}" in
+		g) git_flag=true
+			case "${OPTARG}" in
+				all) update_hash "NODE_BRIDGE_HASH"; update_hash "ETH_BRIDGE_HASH"; update_hash "RELAY_BRIDGE_HASH";;
+				node) update_hash "NODE_BRIDGE_HASH";;
+				relay) update_hash "RELAY_BRIDGE_HASH";;
+				eth) update_hash "ETH_BRIDGE_HASH";;
+				*) echo "Invalid parameter: $1 (expected all/node/relay/eth)"; exit 1;;
+			esac
+			;;
+	esac
+done
+
+compose_extension=''
+if [ "$git_flag" = true ]; then
+	git pull
+	compose_extension='-f docker-compose.yml -f docker-compose.git.yml'
+fi
 
 # Update Matrix access token
 grep -e MATRIX_ACCESS_TOKEN -e WITH_PROXY .env > .env2 && . ./.env2 && rm .env2
@@ -27,8 +43,13 @@ if [ ! -z ${MATRIX_ACCESS_TOKEN+x} ]; then
 	sed -i "s/access_token.*/access_token: \"$MATRIX_ACCESS_TOKEN\"/" ./dashboard/grafana-matrix/config.yml
 fi
 
-# Rebuild images with latest `BRIDGE_HASH`
-docker-compose build
+if [ "$git_flag" = false ]; then
+	# Make sure we grab the latest images from Docker Hub
+	docker-compose pull
+fi
+
+# Rebuild containers with latest images
+docker-compose $compose_extension build
 
 # Stop the proxy cause otherwise the network can't be stopped
 cd ./proxy
@@ -37,9 +58,6 @@ cd -
 
 # Restart the network
 docker-compose down
-
-# Make sure we grab the latest images from Docker Hub
-docker-compose pull
 docker-compose up -d
 
 # Restart the proxy
