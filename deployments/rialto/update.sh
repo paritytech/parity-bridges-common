66e163cba759b71330341aa2de037371ce5616f7
Tomasz Drwięga <tomasz@parity.io>
2020-08-17 12:31:48 +0200

    Allow updating multiple things at once.

--- deployments/rialto/update.sh
+++ deployments/rialto/update.sh
@@ -8,17 +8,28 @@ git pull
 
 # Update *_BRIDGE_HASH in .env
 function update_hash {
+	echo "Updating $1"
 	sed -i "/$1/d" .env || true
 	echo "$1=$(git rev-parse HEAD)" >> .env
 }
 
-case "${1:-all}" in
-	all) update_hash "NODE_BRIDGE_HASH"; update_hash "ETH_BRIDGE_HASH"; update_hash "RELAY_BRIDGE_HASH";;
-	node) update_hash "NODE_BRIDGE_HASH";;
-	relay) update_hash "RELAY_BRIDGE_HASH";;
-	eth) update_hash "ETH_BRIDGE_HASH";;
-	*) echo "Invalid parameter: $1 (expected all/node/relay/eth)"; exit 1;;
-esac
+function process_argument {
+	case "$1" in
+		all) update_hash "NODE_BRIDGE_HASH"; update_hash "ETH_BRIDGE_HASH"; update_hash "RELAY_BRIDGE_HASH";;
+		node) update_hash "NODE_BRIDGE_HASH";;
+		relay) update_hash "RELAY_BRIDGE_HASH";;
+		eth) update_hash "ETH_BRIDGE_HASH";;
+		*) echo "Invalid parameter: $1 (expected all/node/relay/eth)"; exit 1;;
+	esac
+}
+# Run at least once
+process_argument ${1:-all}
+shift || true
+# Then process the rest.
+while [[ $# -gt 0 ]]; do
+	process_argument $1
+	shift
+done
 
 # Update Matrix access token
 grep -e MATRIX_ACCESS_TOKEN -e WITH_PROXY .env > .env2 && . ./.env2 && rm .env2
