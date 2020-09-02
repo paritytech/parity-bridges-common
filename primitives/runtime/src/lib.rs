f10eddb2ba3fc6a1583c26b6228843e751fdcb4a
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-09-02 09:27:24 +0300

    Update primitives/runtime/src/lib.rs
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>

--- primitives/runtime/src/lib.rs
+++ primitives/runtime/src/lib.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Primitives that may be used at (brides) runtime level.
+//! Primitives that may be used at (bridges) runtime level.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
