75c41efd3bb9ed069120df3de196a2df808a57cc
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-09-16 13:00:10 +0300

    extracted relay crates

--- relays/utils/src/lib.rs
+++ relays/utils/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Utilities used by different relays.
+
 use backoff::{backoff::Backoff, ExponentialBackoff};
 use futures::future::FutureExt;
 use std::time::Duration;
