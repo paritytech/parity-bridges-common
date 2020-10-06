d91b0e5c420aafe6e390ab8294b6d1cf000d47a1
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-10-06 17:55:52 +0300

    Update modules/message-lane/src/inbound_lane.rs
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- modules/message-lane/src/inbound_lane.rs
+++ modules/message-lane/src/inbound_lane.rs
@@ -87,7 +87,7 @@ impl<S: InboundLaneStorage> InboundLane<S> {
 			return false;
 		}
 
-		// if there are more unconfirmed messages that we may accept, reject this message
+		// if there are more unconfirmed messages than we may accept, reject this message
 		if self.storage.max_unconfirmed_messages() <= data.relayers.len() as MessageNonce {
 			return false;
 		}
