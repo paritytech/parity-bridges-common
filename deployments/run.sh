02bad6e18b0dadca1846d33ffa02670ca227af8a
Hernando Castano <castano.ha@gmail.com>
2020-11-05 10:26:03 -0500

    Remove uneccessary piping when `cd`-ing in `run.sh`

--- deployments/run.sh
+++ deployments/run.sh
@@ -18,7 +18,7 @@
 set -xeu
 
 # Since the Compose commands are using relative paths we need to `cd` into the `deployments` folder.
-cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 RIALTO=' -f ./networks/rialto.yml'
 MILLAU=' -f ./networks/millau.yml'
