f914cd58331e093813f9313b244bb6b277cfc769
Hernando Castano <castano.ha@gmail.com>
2020-11-11 11:19:30 -0500

    Relay messages from Millau to Rialto

--- deployments/bridges/rialto-millau/entrypoints/relay-messages-millau-to-rialto-entrypoint.sh
+++ deployments/bridges/rialto-millau/entrypoints/relay-messages-millau-to-rialto-entrypoint.sh
@@ -7,9 +7,9 @@ curl -v http://rialto-node-alice:9933/health
 /home/user/substrate-relay millau-messages-to-rialto \
 	--lane 00000000 \
 	--millau-host millau-node-alice \
-	--millau-port 9933 \
+	--millau-port 9944 \
 	--millau-signer //Alice \
 	--rialto-host rialto-node-alice \
-	--rialto-port 9933 \
+	--rialto-port 9944 \
 	--rialto-signer //Alice \
 	--prometheus-host=0.0.0.0
