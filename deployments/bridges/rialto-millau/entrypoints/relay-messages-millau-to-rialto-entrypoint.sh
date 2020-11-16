e0f2e08d0b18719933a43291494bdcf8cf395ee2
Hernando Castano <HCastano@users.noreply.github.com>
2020-11-16 14:18:49 -0500

    Fixes for Millau-Rialto Deployment (#493)
    * Expose two nodes publicly through brucke.link
    
    * Use flags for GNU `sed` instead of BSD `sed`
    
    * Update Substrate relay entrypoint scripts to initialize bridge
    
    * Add Rialto to Millau relay to Compose deployment
    
    * Stop initializing Rialto chain through chainspec
    
    * Include logging for Substrate pallet
    
    * Make Rialto to Millau entrypoint executable
    
    * Use YAML references for relay components
    
    * Use published Substrate Relay image
    
    * Relay messages from Millau to Rialto
    
    * Use Bob nodes to serve message lane
    
    * Fix some port number issues for PoA-Rialto deployment
    
    * Stop directly referencing `environment` anchor in nodes
    
    * Add probable cause to relayer error message
    
    * Edit monitoring config file in-place
    
    * Add some sleep time between bridge init call and starting relays
    
    * Expose grafana.
    
    * Use Root key as bridge pallet owner
    
    In our case that's going to be Alice since she's Root for our Dev
    and Local chains.
    
    Co-authored-by: Tomasz Drwięga <tomasz@parity.io>

---
+++ deployments/bridges/rialto-millau/entrypoints/relay-messages-millau-to-rialto-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xeu
+
+sleep 3
+curl -v http://millau-node-bob:9933/health
+curl -v http://rialto-node-bob:9933/health
+
+MESSAGE_LANE=${MSG_EXCHANGE_GEN_LANE:-00000000}
+
+/home/user/substrate-relay millau-messages-to-rialto \
+	--lane $MESSAGE_LANE \
+	--millau-host millau-node-bob \
+	--millau-port 9944 \
+	--millau-signer //Bob \
+	--rialto-host rialto-node-bob \
+	--rialto-port 9944 \
+	--rialto-signer //Bob \
+	--prometheus-host=0.0.0.0
