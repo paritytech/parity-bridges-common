eeb48b4b13303a7bba372d25da736351dc58038b
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-11-17 15:56:17 +0300

    yml2script

--- scripts/ci-cache.sh
+++ scripts/ci-cache.sh
@@ -2,25 +2,18 @@
 
 set -xeu
 
-HEAD_REF=$1
-TOOLCHAIN=$2
-
-echo "/cache/$HEAD_REF/$TOOLCHAIN/" >> $CARGO_HOME
-echo "/cache/$HEAD_REF/$TOOLCHAIN/" >> $CARGO_TARGET_DIR
-
-
-
+echo $CARGO_TARGET_DIR;
 mkdir -p $CARGO_TARGET_DIR;
 echo "Current Rust nightly version:";
 rustc +nightly --version;
 echo "Cached Rust nightly version:";
 if [ ! -f $CARGO_TARGET_DIR/check_nightly_rust ]; then
-echo "" > $CARGO_TARGET_DIR/check_nightly_rust;
+  echo "" > $CARGO_TARGET_DIR/check_nightly_rust;
 fi
 cat $CARGO_TARGET_DIR/check_nightly_rust;
-if [[ $(cat $CARGO_TARGET_DIR/check_nightly_rust) == $(rustc +nightly --version) ]];
-then echo "The Rust nightly version has not changed";
+if [[ $(cat $CARGO_TARGET_DIR/check_nightly_rust) == $(rustc +nightly --version) ]]; then
+  echo "The Rust nightly version has not changed";
 else
-echo "The Rust nightly version has changed. Clearing the cache";
-rm -rf $CARGO_TARGET_DIR/*;
+  echo "The Rust nightly version has changed. Clearing the cache";
+  rm -rf $CARGO_TARGET_DIR/*;
 fi
