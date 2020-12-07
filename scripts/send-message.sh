e371e7a6fa4bf8af4fd246e506f2a8249593ef1f
Hernando Castano <castano.ha@gmail.com>
2020-12-07 12:13:47 -0500

    Add example usage for send_message script

--- scripts/send-message.sh
+++ scripts/send-message.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Used for manually sending a message to a running network.
+#
+# You could for example spin up a full network using the Docker Compose files
+# we have (to make sure the message relays are running), but remove the message
+# generator service. From there you may submit messages manually using this script.
+
 case "$1" in
 	remark)
 		RUST_LOG=runtime=trace,substrate-relay=trace,bridge=trace \
