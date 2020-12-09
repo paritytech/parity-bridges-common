087c4cfcbb302d1a0c5925ce7908e1cb25fe7766
Hernando Castano <castano.ha@gmail.com>
2020-12-09 11:22:28 -0500

    Update message generator entrypoint script to send transfers

--- deployments/bridges/rialto-millau/entrypoints/relay-messages-to-millau-generator-entrypoint.sh
+++ deployments/bridges/rialto-millau/entrypoints/relay-messages-to-millau-generator-entrypoint.sh
@@ -3,29 +3,82 @@
 # THIS SCRIPT IS NOT INTENDED FOR USE IN PRODUCTION ENVIRONMENT
 #
 # This scripts periodically calls the Substrate relay binary to generate messages. These messages
-# are sent from the Millau network to the Rialto network.
+# are sent from the Rialto network to the Millau network.
 
 set -eu
 
 # Max delay before submitting transactions (s)
-MAX_SUBMIT_DELAY_S=${MSG_EXCHANGE_GEN_MAX_SUBMIT_DELAY_S:-60}
+MAX_SUBMIT_DELAY_S=${MSG_EXCHANGE_GEN_MAX_SUBMIT_DELAY_S:-30}
 MESSAGE_LANE=${MSG_EXCHANGE_GEN_LANE:-00000000}
-MESSAGE=${MSG_EXCHANGE_MESSAGE:-Remark}
+SHARED_ARGS="--rialto-host rialto-node-bob \
+	--rialto-port 9944 \
+	--rialto-signer //Dave \
+	--millau-signer //Dave"
+SEND_MESSAGE="/home/user/substrate-relay submit-rialto-to-millau-message $SHARED_ARGS"
+FERDIE_ADDR=5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL
 
-while true
-do
-	# Sleep a bit between messages
+# Sleep a bit between messages
+rand_sleep() {
 	SUBMIT_DELAY_S=`shuf -i 0-$MAX_SUBMIT_DELAY_S -n 1`
 	echo "Sleeping $SUBMIT_DELAY_S seconds..."
 	sleep $SUBMIT_DELAY_S
+}
 
-	echo "Sending message from Rialto to Millau"
-	/home/user/substrate-relay submit-rialto-to-millau-message \
-		--rialto-host rialto-node-bob \
-		--rialto-port 9944 \
-		--rialto-signer //Dave \
-		--millau-signer //Dave \
+while true
+do
+	rand_sleep
+	echo "Sending Remark from Rialto to Millau using Target Origin"
+	$SEND_MESSAGE \
 		--lane $MESSAGE_LANE \
-		--message $MESSAGE \
-		--fee 100000000
+		--fee 100000000 \
+		--origin Target \
+		remark
+
+	rand_sleep
+	echo "Sending Transfer from Rialto to Millau using Target Origin"
+	 $SEND_MESSAGE \
+		--lane $MESSAGE_LANE \
+		--fee 1000000000 \
+		--origin Target \
+		transfer \
+		--amount 1000000000000 \
+		--recipient $FERDIE_ADDR
+
+	rand_sleep
+	echo "Sending Remark from Rialto to Millau using Source Origin"
+	 $SEND_MESSAGE \
+		--lane $MESSAGE_LANE \
+		--fee 100000000 \
+		--origin Source \
+		remark
+
+	rand_sleep
+	echo "Sending Transfer from Rialto to Millau using Source Origin"
+	 $SEND_MESSAGE \
+		--lane $MESSAGE_LANE \
+		--fee 1000000000 \
+		--origin Source \
+		transfer \
+		--amount 1000000000000 \
+		--recipient $FERDIE_ADDR
+
+	rand_sleep
+	echo "Sending Remark from Rialto to Millau using Root Origin"
+	/home/user/substrate-relay submit-millau-to-rialto-message \
+		$SHARED_ARGS \
+		--lane $MESSAGE_LANE \
+		--fee 100000000 \
+		--origin Root \
+		remark
+
+	rand_sleep
+	echo "Sending Transfer from Rialto to Millau using Root Origin"
+	/home/user/substrate-relay submit-millau-to-rialto-message \
+		$SHARED_ARGS \
+		--lane $MESSAGE_LANE \
+		--fee 1000000000 \
+		--origin Root \
+		transfer \
+		--amount 1000000000000 \
+		--recipient $FERDIE_ADDR
 done
