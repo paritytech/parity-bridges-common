c586ca82094616baf02022ae58513b41977ac334
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-12-18 10:58:20 +0300

    Update modules/message-lane/src/benchmarking.rs
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>

--- modules/message-lane/src/benchmarking.rs
+++ modules/message-lane/src/benchmarking.rs
@@ -248,7 +248,7 @@ benchmarks_instance! {
 		);
 	}
 
-	// Benchmark `receive_messages_delivery_proof` extrinsic with fllowing conditions:
+	// Benchmark `receive_messages_delivery_proof` extrinsic with following conditions:
 	// * single relayer is rewarded for relaying two messages;
 	// * relayer account does not exist (in practice it needs to exist in production environment).
 	//
