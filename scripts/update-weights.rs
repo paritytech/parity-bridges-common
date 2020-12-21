f4b227098e7f53f7a575a9c85e39a291bed905e3
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-12-21 12:34:30 +0300

    automated weights

---
+++ scripts/update-weights.rs
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Run this script from root of the repo
+
+# TODO: Interpreted must be changed to compiled after updating Substrate reference
+
+cargo run --manifest-path=bin/rialto/node/Cargo.toml --release --features=runtime-benchmarks -- benchmark \
+	--chain=local \
+	--steps=50 \
+	--repeat=20 \
+	--pallet=pallet_message_lane \
+	--extrinsic=* \
+	--execution=wasm \
+	--wasm-execution=Interpreted \
+	--heap-pages=4096 \
+	--output=./modules/message-lane/src/weights.rs \
+	--template=./.maintain/weight-template.hbs
