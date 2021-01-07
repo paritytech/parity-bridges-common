261c1b2c5b381731b33749bcbeb5f01fbfb216c3
Hernando Castano <castano.ha@gmail.com>
2021-01-07 17:14:58 -0500

    Sketch out implementation for importing finality proofs

--- modules/finality-verifier/src/lib.rs
+++ modules/finality-verifier/src/lib.rs
@@ -20,16 +20,45 @@
 // Runtime-generated enums
 #![allow(clippy::large_enum_variant)]
 
+use bp_runtime::{BlockNumberOf, Chain, HashOf, HasherOf, HeaderOf};
 use frame_support::{decl_error, decl_module, decl_storage, dispatch::DispatchResult};
+use frame_system::ensure_signed;
+use sp_runtime::traits::Header as HeaderT;
 
-pub trait Config: frame_system::Config {}
+/// Header of the bridged chain.
+pub(crate) type BridgedHeader<T> = HeaderOf<<T as Config>::BridgedChain>;
+
+trait AncestryChecker {
+	type Chain: Chain;
+	type Proof;
+
+	fn are_ancestors(
+		ancestor: <Self::Chain as Chain>::Header,
+		child: <Self::Chain as Chain>::Header,
+		proof: Self::Proof,
+	) -> bool;
+}
+
+trait HeaderChain {
+	type Chain: Chain;
+
+	fn best_finalized() -> <Self::Chain as Chain>::Header;
+}
+
+pub trait Config: frame_system::Config {
+	type BridgedChain: Chain;
+	type HeaderChain: HeaderChain;
+	type AncestryChecker: AncestryChecker;
+}
 
 decl_storage! {
-	trait Store for Module<T: Config> as SubstrateBridge {}
+	trait Store for Module<T: Config> as FinalityVerifier {}
 }
 
 decl_error! {
-	pub enum Error for Module<T: Config> {}
+	pub enum Error for Module<T: Config> {
+		InvalidJustification,
+	}
 }
 
 decl_module! {
@@ -37,8 +66,20 @@ decl_module! {
 		type Error = Error<T>;
 
 		#[weight = 0]
-		fn foo(origin) -> DispatchResult {
-			todo!()
+		fn submit_finality_proof(
+			origin,
+			finality_target: BridgedHeader<T>,
+			justification: Vec<u8>,
+			ancestry_proof: Vec<T::Hash>,
+		) -> DispatchResult {
+			let _ = ensure_signed(origin)?;
+
+			verify_justification().map_err(|_| <Error<T>>::InvalidJustification)?;
+
+			let best_finalized = T::HeaderChain::best_finalized();
+			T::AncestryChecker::are_ancestors(best_finalized, finality_target, ancestry_proof);
+
+			todo!("Write known good headers to storage.")
 		}
 	}
 }
@@ -47,4 +88,8 @@ impl<T: Config> Module<T> {
 	pub fn bar() {
 		todo!()
 	}
+}
+
+fn verify_justification() -> Result<(), ()> {
+	todo!()
 }
