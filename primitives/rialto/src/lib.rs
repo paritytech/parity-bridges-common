75deb44b242a026e8b708e8741198ea19fd339ba
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-01-19 14:15:57 +0300

    include db ops weight in max tx weight estimation

--- primitives/rialto/src/lib.rs
+++ primitives/rialto/src/lib.rs
@@ -65,7 +65,7 @@ pub const MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE: MessageNonce = 128;
 /// This value is a result of `pallet_message_lane::Module::receive_messages_proof` weight formula computation
 /// for the case when single message is delivered. The result then must be rounded up to account possible future
 /// runtime upgrades.
-pub const MAX_SINGLE_MESSAGE_DELIVERY_TX_WEIGHT: Weight = 1_000_000_000;
+pub const MAX_SINGLE_MESSAGE_DELIVERY_TX_WEIGHT: Weight = 1_500_000_000;
 
 /// Maximal weight of single message delivery confirmation transaction on Rialto chain.
 ///
