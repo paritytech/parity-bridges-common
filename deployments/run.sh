87d54671771f82fe0854e7709652da049cc59c90
Tomasz Drwięga <tomasz@parity.io>
2021-01-29 16:52:12 +0100

    Allow disabling the monitoring when starting a test network.

--- deployments/run.sh
+++ deployments/run.sh
@@ -20,6 +20,20 @@ set -xeu
 # Since the Compose commands are using relative paths we need to `cd` into the `deployments` folder.
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+function show_help () {
+  set +x
+  echo " "
+  echo Error: $1
+  echo " "
+  echo "Usage:"
+  echo "  ./run.sh poa-rialto [stop|update]          Run PoA <> Rialto Networks & Bridge"
+  echo "  ./run.sh rialto-millau [stop|update]       Run Rialto <> Millau Networks & Bridge"
+  echo " "
+  echo "Options:"
+  echo "  --no-monitoring                            Disable monitoring"
+  exit 1
+}
+
 RIALTO=' -f ./networks/rialto.yml'
 MILLAU=' -f ./networks/millau.yml'
 ETH_POA=' -f ./networks/eth-poa.yml'
@@ -27,22 +41,43 @@ MONITORING=' -f ./monitoring/docker-compose.yml'
 
 BRIDGE=''
 NETWORKS=''
-case "$1" in
-	poa-rialto)
-		BRIDGE=' -f ./bridges/poa-rialto/docker-compose.yml'
-		NETWORKS+=${RIALTO}
-		NETWORKS+=${ETH_POA}
-		;;
-	rialto-millau)
-		BRIDGE='-f ./bridges/rialto-millau/docker-compose.yml'
-		NETWORKS+=${RIALTO}
-		NETWORKS+=${MILLAU}
-		;;
-	*) echo "Invalid parameter: $1 (expected poa-rialto/rialto-millau)"; exit 1;;
-esac
+SUB_COMMAND='start'
+for i in "$@"
+do
+  case $i in
+    --no-monitoring)
+      MONITORING=" -f ./monitoring/disabled.yml"
+      shift
+      ;;
+    poa-rialto)
+      BRIDGE=$i
+      NETWORKS+=${RIALTO}
+      NETWORKS+=${ETH_POA}
+      shift
+      ;;
+    rialto-millau)
+      BRIDGE=$i
+      NETWORKS+=${RIALTO}
+      NETWORKS+=${MILLAU}
+      shift
+      ;;
+    start|stop|update)
+      SUB_COMMAND=$i
+      shift
+      ;;
+    *)
+      show_help "Unknown option: $i"
+      ;;
+  esac
+done
 
+if [ -z "$BRIDGE" ]; then
+  show_help "Missing bridge name."
+fi
+
+BRIDGE_PATH="./bridges/$BRIDGE"
+BRIDGE="-f $BRIDGE_PATH/docker-compose.yml"
 COMPOSE_FILES=$BRIDGE$NETWORKS$MONITORING
-BRIDGE_PATH="./bridges/$1"
 
 # Compose looks for .env files in the the current directory by default, we don't want that
 COMPOSE_ARGS="--project-directory . --env-file "
@@ -52,42 +87,42 @@ COMPOSE_ARGS+=$BRIDGE_PATH/.env
 grep -e MATRIX_ACCESS_TOKEN -e WITH_PROXY $BRIDGE_PATH/.env > .env2 && . ./.env2 && rm .env2
 
 if [ ! -z ${MATRIX_ACCESS_TOKEN+x} ]; then
-	sed -i "s/access_token.*/access_token: \"$MATRIX_ACCESS_TOKEN\"/" ./monitoring/grafana-matrix/config.yml
+  sed -i "s/access_token.*/access_token: \"$MATRIX_ACCESS_TOKEN\"/" ./monitoring/grafana-matrix/config.yml
 fi
 
 # First check is to see if we have a second argument (since it's optional in this script)
-if [ -n "${2-}" ] && [ "$2" == "stop" ]; then
+if [ "$SUB_COMMAND" == "stop" ]; then
 
-	if [ ! -z ${WITH_PROXY+x} ]; then
-		cd ./reverse-proxy
-		docker-compose down
-		cd -
-	fi
+  if [ ! -z ${WITH_PROXY+x} ]; then
+    cd ./reverse-proxy
+    docker-compose down
+    cd -
+  fi
 
-	docker-compose $COMPOSE_ARGS $COMPOSE_FILES down
+  docker-compose $COMPOSE_ARGS $COMPOSE_FILES down
 
-	exit 0
+  exit 0
 fi
 
-if [ -n "${2-}" ] && [ "$2" == "update" ]; then
+if [ "$SUB_COMMAND" == "update" ]; then
 
-	# Stop the proxy cause otherwise the network can't be stopped
-	if [ ! -z ${WITH_PROXY+x} ]; then
-		cd ./reverse-proxy
-		docker-compose down
-		cd -
-	fi
+  # Stop the proxy cause otherwise the network can't be stopped
+  if [ ! -z ${WITH_PROXY+x} ]; then
+    cd ./reverse-proxy
+    docker-compose down
+    cd -
+  fi
 
 
-	docker-compose $COMPOSE_ARGS $COMPOSE_FILES pull
-	docker-compose $COMPOSE_ARGS $COMPOSE_FILES down
-	docker-compose $COMPOSE_ARGS $COMPOSE_FILES build
+  docker-compose $COMPOSE_ARGS $COMPOSE_FILES pull
+  docker-compose $COMPOSE_ARGS $COMPOSE_FILES down
+  docker-compose $COMPOSE_ARGS $COMPOSE_FILES build
 fi
 
 docker-compose $COMPOSE_ARGS $COMPOSE_FILES up -d
 
 # Start the proxy if needed
 if [ ! -z ${WITH_PROXY+x} ]; then
-	cd ./reverse-proxy
-	docker-compose up -d
+  cd ./reverse-proxy
+  docker-compose up -d
 fi
