790a951c6abfab2e5c8bfc2561bda6e62f1e18da
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-02-01 15:41:34 +0300

    decreased basic delivery tx weight

--- primitives/millau/src/lib.rs
+++ primitives/millau/src/lib.rs
@@ -74,8 +74,7 @@ pub const MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE: MessageNonce = 1024;
 /// This value is a result of `pallet_message_lane::Module::receive_messages_proof` weight formula computation
 /// for the case when single message is delivered. The result then must be rounded up to account possible future
 /// runtime upgrades.
-pub const MAX_SINGLE_MESSAGE_DELIVERY_TX_WEIGHT: Weight = 1_500_000_000;
-
+pub const MAX_SINGLE_MESSAGE_DELIVERY_TX_WEIGHT: Weight = 1_000_000_000;
 /// Increase of delivery transaction weight on Millau chain with every additional message byte.
 pub const ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT: Weight = 3_000;
 
