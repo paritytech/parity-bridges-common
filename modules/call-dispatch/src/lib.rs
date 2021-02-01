757ff49b78d24d9973b6552d63ea833baec5aff4
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-02-01 15:45:44 +0300

    fmt

--- modules/call-dispatch/src/lib.rs
+++ modules/call-dispatch/src/lib.rs
@@ -525,7 +525,7 @@ mod tests {
 
 	impl Filter<Call> for TestCallFilter {
 		fn filter(call: &Call) -> bool {
-			!matches!(*call,Call::System(frame_system::Call::fill_block(_)))
+			!matches!(*call, Call::System(frame_system::Call::fill_block(_)))
 		}
 	}
 
