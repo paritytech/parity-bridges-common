cce2044f217017cd27185c94a2c6f5ec8c0690b7
adoerr <0xad@gmx.net>
2021-02-22 16:26:42 +0100

    cargo fmt --all

--- bin/millau/node/src/command.rs
+++ bin/millau/node/src/command.rs
@@ -70,10 +70,9 @@ impl SubstrateCli for Cli {
 pub fn run() -> sc_cli::Result<()> {
 	let cli = Cli::from_args();
 	// make sure to set correct crypto version.
-	sp_core::crypto::set_default_ss58_version(
-		sp_core::crypto::Ss58AddressFormat::Custom(millau_runtime::SS58Prefix::get() as u16)
-	);
-
+	sp_core::crypto::set_default_ss58_version(sp_core::crypto::Ss58AddressFormat::Custom(
+		millau_runtime::SS58Prefix::get() as u16,
+	));
 
 	match &cli.subcommand {
 		Some(Subcommand::Benchmark(cmd)) => {
