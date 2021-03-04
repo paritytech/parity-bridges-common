30f292504bb00e3d1ca54a0bb8ac942333e466af
Hector Bulgarini <hbulgarini@gmail.com>
2021-03-04 16:34:19 -0300

    Fixing send message script from millau to rialot

--- scripts/send-message-from-millau-rialto.sh
+++ scripts/send-message-from-millau-rialto.sh
@@ -6,12 +6,14 @@
 # we have (to make sure the message relays are running), but remove the message
 # generator service. From there you may submit messages manually using this script.
 
+MILLAU_PORT="${RIALTO_PORT:-9945}"
+
 case "$1" in
 	remark)
 		RUST_LOG=runtime=trace,substrate-relay=trace,bridge=trace \
 		./target/debug/substrate-relay send-message millau-to-rialto \
 			--millau-host localhost \
-			--millau-port 9944 \
+			--millau-port $MILLAU_PORT \
 			--millau-signer //Alice \
 			--rialto-signer //Bob \
 			--lane 00000000 \
@@ -22,7 +24,7 @@ case "$1" in
 		RUST_LOG=runtime=trace,substrate-relay=trace,bridge=trace \
 		./target/debug/substrate-relay send-message millau-to-rialto \
 			--millau-host localhost \
-			--millau-port 9945 \
+			--millau-port $MILLAU_PORT \
 			--millau-signer //Alice \
 			--rialto-signer //Bob \
 			--lane 00000000 \
