d0554397d1877368579868cf58af3df75c68993e
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-03-23 16:11:23 +0300

    slightly changed relay loop initialization

--- relays/ethereum/src/rpc_errors.rs
+++ relays/ethereum/src/rpc_errors.rs
@@ -29,6 +29,8 @@ pub enum RpcError {
 	Ethereum(EthereumNodeError),
 	/// An error occured when interacting with a Substrate node.
 	Substrate(SubstrateNodeError),
+	/// Error running relay loop.
+	Loop(String),
 }
 
 impl From<RpcError> for String {
@@ -37,6 +39,7 @@ impl From<RpcError> for String {
 			RpcError::Serialization(e) => e.to_string(),
 			RpcError::Ethereum(e) => e.to_string(),
 			RpcError::Substrate(e) => e.to_string(),
+			RpcError::Loop(e) => e,
 		}
 	}
 }
