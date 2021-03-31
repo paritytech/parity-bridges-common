2f4fd603717b72d3c0d1220aac9689500c2f4082
Tomasz Drwięga <tomasz@parity.io>
2021-03-31 16:40:38 +0200

    Add docs.

--- relays/bin-substrate/src/cli/mod.rs
+++ relays/bin-substrate/src/cli/mod.rs
@@ -290,30 +290,43 @@ impl AccountId {
 	}
 }
 
-// TODO [ToDr] Docs.
+/// Bridge-supported network definition.
+///
+/// Used to abstract away CLI commands.
 pub trait CliChain: relay_substrate_client::Chain {
+	/// Chain's current version of the runtime.
 	const RUNTIME_VERSION: sp_version::RuntimeVersion;
 
+	/// Crypto keypair type used to send messages.
+	///
+	/// In case of chains supporting multiple cryptos, pick one used by the CLI.
 	type KeyPair: sp_core::crypto::Pair;
 
-	// TODO [ToDr] This has to be replaced with Target-specific payload.
+	/// Bridge Message Payload type.
+	///
+	/// TODO [ToDr] This should be removed in favour of target-specifc types.
 	type MessagePayload;
 
 	/// Numeric value of SS58 format.
 	fn ss58_format() -> u16;
 
+	/// Parse CLI call and encode it to be dispatched on this specific chain.
 	fn encode_call(call: crate::rialto_millau::cli::Call) -> Result<Self::Call, String>;
 
+	/// Construct message payload to be sent over the bridge.
 	fn encode_message(message: crate::rialto_millau::cli::MessagePayload) -> Result<Self::MessagePayload, String>;
 
+	/// Maximal extrinsic weight (from the runtime).
 	fn max_extrinsic_weight() -> Weight;
 
+	/// Convert CLI signing parameters of `Source` chain into a `KeyPair` instance.
 	fn source_signing_params(params: SourceSigningParams) -> Result<Self::KeyPair, String> {
 		use sp_core::crypto::Pair;
 		Self::KeyPair::from_string(&params.source_signer, params.source_signer_password.as_deref())
 			.map_err(|e| format!("Failed to parse source-signer: {:?}", e))
 	}
 
+	/// Convert CLI signing parameters of `Target` chain into a `KeyPair` instance.
 	fn target_signing_params(params: TargetSigningParams) -> Result<Self::KeyPair, String> {
 		use sp_core::crypto::Pair;
 		Self::KeyPair::from_string(&params.target_signer, params.target_signer_password.as_deref())
