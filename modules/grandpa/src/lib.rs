51a96539e0cb61cc2dfd172134800a349a39393d
Hernando Castano <hernando@hcastano.com>
2021-04-01 16:23:48 -0400

    Use more accurate weight calculation in declared weight

--- modules/grandpa/src/lib.rs
+++ modules/grandpa/src/lib.rs
@@ -44,7 +44,6 @@ use codec::{Decode, Encode};
 use finality_grandpa::voter_set::VoterSet;
 use frame_support::ensure;
 use frame_system::{ensure_signed, RawOrigin};
-use num_traits::cast::AsPrimitive;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
 use sp_finality_grandpa::{ConsensusLog, GRANDPA_ENGINE_ID};
@@ -140,9 +139,15 @@ pub mod pallet {
 		///
 		/// If successful in verification, it will write the target header to the underlying storage
 		/// pallet.
+		// Note that the number of precommits is indicitive of the number of GRANDPA forks being
+		// voted on.
+		//
+		// This represents the average number of votes in a single fork since the weight formula
+		// uses that metric instead of the aggregated number of vote ancestries.
 		#[pallet::weight(T::WeightInfo::submit_finality_proof(
-			T::MaxBridgedSessionLength::get().as_() as u32,
-			T::MaxBridgedValidatorCount::get(),
+			justification.commit.precommits.len() as u32,
+			((justification.votes_ancestries.len() + justification.commit.precommits.len() - 1)
+				/ justification.commit.precommits.len()) as u32,
 		))]
 		pub fn submit_finality_proof(
 			origin: OriginFor<T>,
@@ -181,15 +186,7 @@ pub mod pallet {
 
 			log::info!(target: "runtime::bridge-grandpa", "Succesfully imported finalized header with hash {:?}!", hash);
 
-			// Note that the number of precommits is indicitive of the number of GRANDPA forks being
-			// voted on.
-			let precommits = justification.commit.precommits.len();
-
-			// This represents the average number of votes in a single fork since the weight formula
-			// uses that metric instead of the aggregated number of vote ancestries.
-			let votes = (justification.votes_ancestries.len() + precommits - 1) / precommits;
-
-			Ok(Some(T::WeightInfo::submit_finality_proof(votes as u32, precommits as u32)).into())
+			Ok(().into())
 		}
 
 		/// Bootstrap the bridge pallet with an initial header and authority set from which to sync.
