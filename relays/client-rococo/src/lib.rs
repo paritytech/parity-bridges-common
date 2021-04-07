ca2116a0866bc0c6ab58414053c6f6f28e9e616e
Hernando Castano <hernando@hcastano.com>
2021-04-07 12:25:24 -0400

    Add missing part of license header

--- relays/client-rococo/src/lib.rs
+++ relays/client-rococo/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
 // Parity Bridges Common is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
