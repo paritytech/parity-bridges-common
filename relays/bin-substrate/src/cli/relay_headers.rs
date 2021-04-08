62aca80e8cc8fb8e982e5fd486492bbe7ac41dc8
Hernando Castano <hernando@hcastano.com>
2021-04-07 20:57:38 -0400

    Add Westend<>Rococo variants to `relay_headers`

--- relays/bin-substrate/src/cli/relay_headers.rs
+++ relays/bin-substrate/src/cli/relay_headers.rs
@@ -42,6 +42,8 @@ arg_enum! {
 		MillauToRialto,
 		RialtoToMillau,
 		WestendToMillau,
+		WestendToRococo,
+		RococoToWestend,
 	}
 }
 
@@ -59,12 +61,28 @@ macro_rules! select_bridge {
 				type Source = relay_rialto_client::Rialto;
 				type Target = relay_millau_client::Millau;
 				type Finality = crate::rialto_millau::rialto_headers_to_millau::RialtoFinalityToMillau;
+
 				$generic
 			}
 			RelayHeadersBridge::WestendToMillau => {
 				type Source = relay_westend_client::Westend;
 				type Target = relay_millau_client::Millau;
 				type Finality = crate::rialto_millau::westend_headers_to_millau::WestendFinalityToMillau;
+
+				$generic
+			}
+			RelayHeadersBridge::WestendToRococo => {
+				type Source = relay_westend_client::Westend;
+				type Target = relay_rococo_client::Rococo;
+				type Finality = crate::rialto_millau::westend_headers_to_rococo::WestendFinalityToRococo;
+
+				$generic
+			}
+			RelayHeadersBridge::RococoToWestend => {
+				type Source = relay_rococo_client::Rococo;
+				type Target = relay_westend_client::Westend;
+				type Finality = crate::rialto_millau::rococo_headers_to_westend::RococoFinalityToWestend;
+
 				$generic
 			}
 		}
