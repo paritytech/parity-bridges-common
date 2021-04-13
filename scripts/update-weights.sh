d950a92b66c524be082a99f9412bceaccbacfdda
Tomasz Drwięga <tomasz@parity.io>
2021-04-13 16:25:40 +0200

    Update weights script.

--- scripts/update-weights.sh
+++ scripts/update-weights.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+set -exu
+
 # Run this script from root of the repo
 
-cargo run --manifest-path=bin/rialto/node/Cargo.toml --release --features=runtime-benchmarks -- benchmark \
+time cargo run --manifest-path=bin/rialto/node/Cargo.toml --release --features=runtime-benchmarks -- benchmark \
 	--chain=local \
 	--steps=50 \
 	--repeat=20 \
@@ -12,4 +14,16 @@ cargo run --manifest-path=bin/rialto/node/Cargo.toml --release --features=runtim
 	--wasm-execution=Compiled \
 	--heap-pages=4096 \
 	--output=./modules/messages/src/weights.rs \
+	--template=./.maintain/rialto-weight-template.hbs
+
+time cargo run --manifest-path=bin/rialto/node/Cargo.toml --release --features=runtime-benchmarks -- benchmark \
+	--chain=local \
+	--steps=50 \
+	--repeat=20 \
+	--pallet=pallet_bridge_grandpa \
+	--extrinsic=* \
+	--execution=wasm \
+	--wasm-execution=Compiled \
+	--heap-pages=4096 \
+	--output=./modules/grandpa/src/weights.rs \
 	--template=./.maintain/rialto-weight-template.hbs
