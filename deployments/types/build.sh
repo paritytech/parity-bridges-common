218189d8056055ac93a8e7a47396e792cda81b75
Hernando Castano <hernando@hcastano.com>
2021-05-21 12:13:42 -0400

    Add Rococo/Wococo support to types build script

--- deployments/types/build.sh
+++ deployments/types/build.sh
@@ -5,11 +5,13 @@
 # Both networks have a lot of common types, so to avoid duplication we merge `common.json` file with
 # chain-specific definitions in `rialto|millau.json`.
 
-set -exu
+set -eux
 
 # Make sure we are in the right dir.
 cd $(dirname $(realpath $0))
 
-# Create rialto and millau types.
+# Create types for our supported bridge pairs (Rialto<>Millau, Rococo<>Wococo)
 jq -s '.[0] * .[1]' common.json rialto.json > ../types-rialto.json
 jq -s '.[0] * .[1]' common.json millau.json > ../types-millau.json
+jq -s '.[0] * .[1]' common.json rococo.json > ../types-rococo.json
+jq -s '.[0] * .[1]' common.json wococo.json > ../types-wococo.json
