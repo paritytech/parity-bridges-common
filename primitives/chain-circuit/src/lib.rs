30b48535d3b06b0a15b25795ca738d58d19ab5e8
MaciejBaj <macie.baj@gmail.com>
2021-06-08 09:57:22 +0200

    Use Blake2_256 as hashing and u128 as values for Circuit primitives

--- primitives/chain-circuit/src/lib.rs
+++ primitives/chain-circuit/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 Parity Technologies (UK) Ltd.
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
 // This file is part of Parity Bridges Common.
 
 // Parity Bridges Common is free software: you can redistribute it and/or modify
@@ -20,8 +20,6 @@
 // Runtime-generated DecodeLimit::decode_all_With_depth_limit
 #![allow(clippy::unnecessary_mut_passed)]
 
-mod circuit_hash;
-
 use bp_messages::{LaneId, MessageNonce, UnrewardedRelayersState};
 use bp_runtime::Chain;
 use frame_support::{
@@ -32,16 +30,10 @@ use frame_system::limits;
 use sp_core::Hasher as HasherT;
 use sp_runtime::traits::Convert;
 use sp_runtime::{
-	traits::{IdentifyAccount, Verify},
+	traits::{BlakeTwo256, IdentifyAccount, Verify},
 	MultiSignature, MultiSigner, Perbill,
 };
 use sp_std::prelude::*;
-use sp_trie::{trie_types::Layout, TrieConfiguration};
-
-#[cfg(feature = "std")]
-use serde::{Deserialize, Serialize};
-
-pub use circuit_hash::CircuitHash;
 
 /// Number of extra bytes (excluding size of storage value itself) of storage proof, built at
 /// Circuit chain. This mostly depends on number of entries (and their density) in the storage trie.
@@ -76,7 +68,7 @@ pub const MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE: MessageNonce = 1024;
 
 /// Weight of single regular message delivery transaction on Circuit chain.
 ///
-/// This value is a result of `pallet_bridge_messages::Module::receive_messages_proof_weight()` call
+/// This value is a result of `pallet_bridge_messages::Pallet::receive_messages_proof_weight()` call
 /// for the case when single message of `pallet_bridge_messages::EXPECTED_DEFAULT_MESSAGE_LENGTH` bytes is delivered.
 /// The message must have dispatch weight set to zero. The result then must be rounded up to account
 /// possible future runtime upgrades.
@@ -90,12 +82,16 @@ pub const ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT: Weight = 25_000;
 
 /// Maximal weight of single message delivery confirmation transaction on Circuit chain.
 ///
-/// This value is a result of `pallet_bridge_messages::Module::receive_messages_delivery_proof` weight formula computation
+/// This value is a result of `pallet_bridge_messages::Pallet::receive_messages_delivery_proof` weight formula computation
 /// for the case when single message is confirmed. The result then must be rounded up to account possible future
 /// runtime upgrades.
 pub const MAX_SINGLE_MESSAGE_DELIVERY_CONFIRMATION_TX_WEIGHT: Weight = 2_000_000_000;
 
-/// The length of a session (how often authorities change) on Circuit measured in of number of blocks.
+/// The target length of a session (how often authorities change) on Circuit measured in of number of
+/// blocks.
+///
+/// Note that since this is a target sessions may change before/after this time depending on network
+/// conditions.
 pub const SESSION_LENGTH: BlockNumber = 5 * time_units::MINUTES;
 
 /// Re-export `time_units` to make usage easier.
@@ -117,10 +113,10 @@ pub mod time_units {
 pub type BlockNumber = u64;
 
 /// Hash type used in Circuit.
-pub type Hash = <BlakeTwoAndKeccak256 as HasherT>::Out;
+pub type Hash = <BlakeTwo256 as HasherT>::Out;
 
 /// The type of an object that can produce hashes on Circuit.
-pub type Hasher = BlakeTwoAndKeccak256;
+pub type Hasher = BlakeTwo256;
 
 /// The header type used by Circuit.
 pub type Header = sp_runtime::generic::Header<BlockNumber, Hasher>;
@@ -136,7 +132,7 @@ pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::Account
 pub type AccountSigner = MultiSigner;
 
 /// Balance of an account.
-pub type Balance = u64;
+pub type Balance = u128;
 
 /// Circuit chain.
 #[derive(RuntimeDebug)]
@@ -149,37 +145,6 @@ impl Chain for Circuit {
 	type Header = Header;
 }
 
-
-/// Circuit Hasher (Blake2-256 ++ Keccak-256) implementation.
-#[derive(PartialEq, Eq, Clone, Copy, RuntimeDebug)]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-pub struct BlakeTwoAndKeccak256;
-
-impl sp_core::Hasher for BlakeTwoAndKeccak256 {
-	type Out = CircuitHash;
-	type StdHasher = hash256_std_hasher::Hash256StdHasher;
-	const LENGTH: usize = 64;
-
-	fn hash(s: &[u8]) -> Self::Out {
-		let mut combined_hash = CircuitHash::default();
-		combined_hash.as_mut()[..32].copy_from_slice(&sp_io::hashing::blake2_256(s));
-		combined_hash.as_mut()[32..].copy_from_slice(&sp_io::hashing::keccak_256(s));
-		combined_hash
-	}
-}
-
-impl sp_runtime::traits::Hash for BlakeTwoAndKeccak256 {
-	type Output = CircuitHash;
-
-	fn trie_root(input: Vec<(Vec<u8>, Vec<u8>)>) -> Self::Output {
-		Layout::<BlakeTwoAndKeccak256>::trie_root(input)
-	}
-
-	fn ordered_trie_root(input: Vec<Vec<u8>>) -> Self::Output {
-		Layout::<BlakeTwoAndKeccak256>::ordered_trie_root(input)
-	}
-}
-
 /// Convert a 256-bit hash into an AccountId.
 pub struct AccountIdConverter;
 
@@ -189,7 +154,7 @@ impl sp_runtime::traits::Convert<sp_core::H256, AccountId> for AccountIdConverte
 	}
 }
 
-/// We use this to get the account on Circuit (target) which is derived from Rialto's (source)
+/// We use this to get the account on Circuit (target) which is derived from Gateway (source)
 /// account. We do this so we can fund the derived account on Circuit at Genesis to it can pay
 /// transaction fees.
 ///
@@ -198,7 +163,7 @@ impl sp_runtime::traits::Convert<sp_core::H256, AccountId> for AccountIdConverte
 ///
 /// Note that this should only be used for testing.
 pub fn derive_account_from_gateway_id(id: bp_runtime::SourceAccount<AccountId>) -> AccountId {
-	let encoded_id = bp_runtime::derive_account_id(bp_runtime::GATEWAY_BRIDGE_INSTANCE, id);
+	let encoded_id = bp_runtime::derive_account_id(bp_runtime::GATEWAY_CHAIN_ID, id);
 	AccountIdConverter::convert(encoded_id)
 }
 
@@ -256,31 +221,6 @@ pub const FROM_CIRCUIT_LATEST_CONFIRMED_NONCE_METHOD: &str = "FromCircuitInbound
 pub const FROM_CIRCUIT_UNREWARDED_RELAYERS_STATE: &str = "FromCircuitInboundLaneApi_unrewarded_relayers_state";
 
 sp_api::decl_runtime_apis! {
-	/// API for querying information about Circuit headers from the Bridge Pallet instance.
-	///
-	/// This API is implemented by runtimes that are bridging with the Circuit chain, not the
-	/// Circuit runtime itself.
-	pub trait CircuitHeaderApi {
-		/// Returns number and hash of the best blocks known to the bridge module.
-		///
-		/// Will return multiple headers if there are many headers at the same "best" height.
-		///
-		/// The caller should only submit an `import_header` transaction that makes
-		/// (or leads to making) other header the best one.
-		fn best_blocks() -> Vec<(BlockNumber, Hash)>;
-		/// Returns number and hash of the best finalized block known to the bridge module.
-		fn finalized_block() -> (BlockNumber, Hash);
-		/// Returns numbers and hashes of headers that require finality proofs.
-		///
-		/// An empty response means that there are no headers which currently require a
-		/// finality proof.
-		fn incomplete_headers() -> Vec<(BlockNumber, Hash)>;
-		/// Returns true if the header is known to the runtime.
-		fn is_known_block(hash: Hash) -> bool;
-		/// Returns true if the header is considered finalized by the runtime.
-		fn is_finalized_block(hash: Hash) -> bool;
-	}
-
 	/// API for querying information about the finalized Circuit headers.
 	///
 	/// This API is implemented by runtimes that are bridging with the Circuit chain, not the
