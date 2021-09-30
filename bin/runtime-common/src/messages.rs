6d937dec88d5f4184530cc960eedfb36a30e4338
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-09-30 15:13:31 +0300

    fixed origin verification

--- bin/runtime-common/src/messages.rs
+++ bin/runtime-common/src/messages.rs
@@ -269,7 +269,6 @@ pub mod source {
 		"Unable to match the source origin to expected target origin.";
 	pub(crate) const TOO_LOW_FEE: &str =
 		"Provided fee is below minimal threshold required by the lane.";
-	pub(crate) const INVALID_SUBMITTER_ORIGIN: &str = "The submitter origin is invalid.";
 
 	impl<B>
 		LaneMessageVerifier<
@@ -314,11 +313,20 @@ pub mod source {
 				frame_system::RawOrigin<AccountIdOf<ThisChain<B>>>,
 				OriginOf<ThisChain<B>>,
 			> = submitter.clone().into();
-			pallet_bridge_dispatch::verify_message_origin(
-				&raw_origin_or_err.map_err(|_| INVALID_SUBMITTER_ORIGIN)?,
-				payload,
-			)
-			.map_err(|_| BAD_ORIGIN)?;
+			match raw_origin_or_err {
+				Ok(raw_origin) => pallet_bridge_dispatch::verify_message_origin(
+					&raw_origin,
+					payload,
+				)
+				.map(drop)
+				.map_err(|_| BAD_ORIGIN)?,
+				Err(_) => {
+					// so what it means that we've failed to convert origin to the `frame_system::RawOrigin`?
+					// now it means that the custom pallet origin has been used to send the message. Do we need
+					// to verify it? The answer is no, because pallet may craft any origin (e.g. root) && we
+					// can't verify whether it is valid, or not.
+				},
+			};
 
 			let minimal_fee_in_this_tokens =
 				estimate_message_dispatch_and_delivery_fee::<B>(payload, B::RELAYER_FEE_PERCENT)?;
