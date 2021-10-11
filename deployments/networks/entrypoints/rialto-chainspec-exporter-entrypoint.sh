1eee0172d7ee4a320ef292eeaf2cea52c93157e6
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-10-11 16:33:39 +0300

    added some docs

--- deployments/networks/entrypoints/rialto-chainspec-exporter-entrypoint.sh
+++ deployments/networks/entrypoints/rialto-chainspec-exporter-entrypoint.sh
@@ -7,6 +7,8 @@ set -xeu
 	--disable-default-bootnode \
 	> /rialto-share/rialto-relaychain-spec-raw.json
 
-ls /rialto-share
-
+# we're using local driver + tmpfs for shared `/rialto-share` volume, which is populated
+# by the container running this script. If this script ends, the volume will be detached
+# and our chain spec will be lost when it'll go online again. Hence the never-ending
+# script which keeps volume online until container is stopped.
 tail -f /dev/null
