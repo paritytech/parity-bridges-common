6e47b19da533e574e4f859e15e6c8bab3005f08c
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-06-07 10:50:55 +0300

    Parachains pallet benchmarks (#1436)
    * added parachains pallet benchmarks
    
    * deduplicate insertion of bridged header
    
    * pruning weight
    
    * fixes
    
    * fix compilation

--- scripts/update-weights.sh
+++ scripts/update-weights.sh
@@ -6,7 +6,7 @@
 
 set -eux
 
-time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- benchmark \
+time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- benchmark pallet \
 	--chain=dev \
 	--steps=50 \
 	--repeat=20 \
@@ -18,7 +18,7 @@ time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- 
 	--output=./modules/messages/src/weights.rs \
 	--template=./.maintain/millau-weight-template.hbs
 
-time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- benchmark \
+time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- benchmark pallet \
 	--chain=dev \
 	--steps=50 \
 	--repeat=20 \
@@ -28,4 +28,16 @@ time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- 
 	--wasm-execution=Compiled \
 	--heap-pages=4096 \
 	--output=./modules/grandpa/src/weights.rs \
+	--template=./.maintain/millau-weight-template.hbs
+
+time cargo run --release -p millau-bridge-node --features=runtime-benchmarks -- benchmark pallet \
+	--chain=dev \
+	--steps=50 \
+	--repeat=20 \
+	--pallet=pallet_bridge_parachains \
+	--extrinsic=* \
+	--execution=wasm \
+	--wasm-execution=Compiled \
+	--heap-pages=4096 \
+	--output=./modules/parachains/src/weights.rs \
 	--template=./.maintain/millau-weight-template.hbs
