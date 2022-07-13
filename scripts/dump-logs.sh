d3bec3daa2bdf4f60d2405bea06a99c7e55585e1
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-07-13 15:16:05 +0300

    fix dump-logs

--- scripts/dump-logs.sh
+++ scripts/dump-logs.sh
@@ -24,7 +24,8 @@ SERVICES=(\
 	deployments_relay-parachains-westend-to-millau-1_2 \
 	deployments_relay-messages-millau-to-rialto-parachain-generator_1 \
 	deployments_relay-messages-rialto-parachain-to-millau-generator_1 \
-	deployments_relay-millau-rialto-parachain_1 \
+	deployments_relay-millau-rialto-parachain-1_1 \
+	deployments_relay-millau-rialto-parachain-2_1 \
 	deployments_rialto-node-alice_1 \
 	deployments_rialto-node-bob_1 \
 	deployments_millau-node-alice_1 \
