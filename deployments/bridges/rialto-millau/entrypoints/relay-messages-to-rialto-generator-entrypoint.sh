396a0f99241e6d0defdd98ea0cd678357b8f8362
Serban Iorga <serban@parity.io>
2022-08-05 09:21:03 +0300

    [Deployments] Deduplicate message sending code (#1537)
    * [Deployments] Deduplicate message sending code
    
    Signed-off-by: Serban Iorga <serban@parity.io>
    
    * [Deployments] README.md fixes

--- deployments/bridges/rialto-millau/entrypoints/relay-messages-to-rialto-generator-entrypoint.sh
+++ deployments/bridges/rialto-millau/entrypoints/relay-messages-to-rialto-generator-entrypoint.sh
@@ -12,7 +12,6 @@ MAX_SUBMIT_DELAY_S=${MSG_EXCHANGE_GEN_MAX_SUBMIT_DELAY_S:-30}
 MESSAGE_LANE=${MSG_EXCHANGE_GEN_LANE:-00000000}
 SECONDARY_MESSAGE_LANE=${MSG_EXCHANGE_GEN_SECONDARY_LANE}
 MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE=128
-FERDIE_ADDR=6ztG3jPnJTwgZnnYsgCDXbbQVR82M96hBZtPvkN56A9668ZC
 
 SHARED_CMD=" /home/user/substrate-relay send-message millau-to-rialto"
 SHARED_HOST="--source-host millau-node-bob --source-port 9944"
@@ -20,84 +19,10 @@ SOURCE_SIGNER="--source-signer //Rialto.MessagesSender"
 
 SEND_MESSAGE="$SHARED_CMD $SHARED_HOST $SOURCE_SIGNER"
 
-# Sleep a bit between messages
-rand_sleep() {
-	SUBMIT_DELAY_S=`shuf -i 0-$MAX_SUBMIT_DELAY_S -n 1`
-	echo "Sleeping $SUBMIT_DELAY_S seconds..."
-	sleep $SUBMIT_DELAY_S
-	NOW=`date "+%Y-%m-%d %H:%M:%S"`
-	echo "Woke up at $NOW"
-}
+SOURCE_CHAIN="Millau"
+TARGET_CHAIN="Rialto"
+EXTRA_ARGS="--use-xcm-pallet"
+REGULAR_PAYLOAD="020419ac"
+BATCH_PAYLOAD="020419ac"
 
-# last time when we have been asking for conversion rate update
-LAST_CONVERSION_RATE_UPDATE_TIME=0
-# conversion rate override argument
-CONVERSION_RATE_OVERRIDE="--conversion-rate-override metric"
-
-# start sending large messages immediately
-LARGE_MESSAGES_TIME=0
-# start sending message packs in a hour
-BUNCH_OF_MESSAGES_TIME=3600
-
-while true
-do
-	rand_sleep
-
-	# ask for latest conversion rate. We're doing that because otherwise we'll be facing
-	# bans from the conversion rate provider
-	if [ $SECONDS -ge $LAST_CONVERSION_RATE_UPDATE_TIME ]; then
-		CONVERSION_RATE_OVERRIDE="--conversion-rate-override metric"
-		CONVERSION_RATE_UPDATE_DELAY=`shuf -i 300-600 -n 1`
-		LAST_CONVERSION_RATE_UPDATE_TIME=$((SECONDS + $CONVERSION_RATE_UPDATE_DELAY))
-	fi
-
-	# send regular message
-	echo "Sending Message from Millau to Rialto"
-	SEND_MESSAGE_OUTPUT=`$SEND_MESSAGE --lane $MESSAGE_LANE $CONVERSION_RATE_OVERRIDE --use-xcm-pallet raw 020419ac 2>&1`
-	echo $SEND_MESSAGE_OUTPUT
-	if [ "$CONVERSION_RATE_OVERRIDE" = "--conversion-rate-override metric" ]; then
-		ACTUAL_CONVERSION_RATE_REGEX="conversion rate override: ([0-9\.]+)"
-		if [[ $SEND_MESSAGE_OUTPUT =~ $ACTUAL_CONVERSION_RATE_REGEX ]]; then
-			CONVERSION_RATE=${BASH_REMATCH[1]}
-			echo "Read updated conversion rate: $CONVERSION_RATE"
-			CONVERSION_RATE_OVERRIDE="--conversion-rate-override $CONVERSION_RATE"
-		else
-			echo "Error: unable to find conversion rate in send-message output. Will keep using on-chain rate"
-			CONVERSION_RATE_OVERRIDE=""
-		fi
-	fi
-
-	if [ ! -z $SECONDARY_MESSAGE_LANE ]; then
-		echo "Sending Message from Millau to Rialto using secondary lane: $SECONDARY_MESSAGE_LANE"
-		$SEND_MESSAGE \
-			--lane $SECONDARY_MESSAGE_LANE \
-			$CONVERSION_RATE_OVERRIDE \
-			raw 020419ac
-	fi
-
-	# every other hour we're sending 3 large (size, weight, size+weight) messages
-	if [ $SECONDS -ge $LARGE_MESSAGES_TIME ]; then
-		LARGE_MESSAGES_TIME=$((SECONDS + 7200))
-
-		rand_sleep
-		echo "Sending Maximal Size Message from Millau to Rialto"
-		$SEND_MESSAGE \
-			--lane $MESSAGE_LANE \
-			$CONVERSION_RATE_OVERRIDE \
-			sized max
-	fi
-
-	# every other hour we're sending a bunch of small messages
-	if [ $SECONDS -ge $BUNCH_OF_MESSAGES_TIME ]; then
-		BUNCH_OF_MESSAGES_TIME=$((SECONDS + 7200))
-		for i in $(seq 0 $MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE);
-		do
-			$SEND_MESSAGE \
-				--lane $MESSAGE_LANE \
-				--use-xcm-pallet \
-				$CONVERSION_RATE_OVERRIDE \
-				raw 020419ac
-		done
-
-	fi
-done
+source /common/generate_messages.sh
--- deployments/bridges/rialto-parachain-millau/entrypoints/relay-messages-to-millau-generator-entrypoint.sh
+++ deployments/bridges/rialto-parachain-millau/entrypoints/relay-messages-to-millau-generator-entrypoint.sh
@@ -18,74 +18,10 @@ SOURCE_SIGNER="--source-signer //Millau.MessagesSender"
 
 SEND_MESSAGE="$SHARED_CMD $SHARED_HOST $SOURCE_SIGNER"
 
-# Sleep a bit between messages
-rand_sleep() {
-	SUBMIT_DELAY_S=`shuf -i 0-$MAX_SUBMIT_DELAY_S -n 1`
-	echo "Sleeping $SUBMIT_DELAY_S seconds..."
-	sleep $SUBMIT_DELAY_S
-	NOW=`date "+%Y-%m-%d %H:%M:%S"`
-	echo "Woke up at $NOW"
-}
+SOURCE_CHAIN="RialtoParachain"
+TARGET_CHAIN="Millau"
+EXTRA_ARGS="--use-xcm-pallet"
+REGULAR_PAYLOAD="020419ac"
+BATCH_PAYLOAD="010109020419A8"
 
-# last time when we have been asking for conversion rate update
-LAST_CONVERSION_RATE_UPDATE_TIME=0
-# conversion rate override argument
-CONVERSION_RATE_OVERRIDE="--conversion-rate-override metric"
-
-# start sending large messages immediately
-LARGE_MESSAGES_TIME=0
-# start sending message packs in a hour
-BUNCH_OF_MESSAGES_TIME=3600
-
-while true
-do
-	rand_sleep
-
-	# ask for latest conversion rate. We're doing that because otherwise we'll be facing
-	# bans from the conversion rate provider
-	if [ $SECONDS -ge $LAST_CONVERSION_RATE_UPDATE_TIME ]; then
-		CONVERSION_RATE_OVERRIDE="--conversion-rate-override metric"
-		CONVERSION_RATE_UPDATE_DELAY=`shuf -i 300-600 -n 1`
-		LAST_CONVERSION_RATE_UPDATE_TIME=$((SECONDS + $CONVERSION_RATE_UPDATE_DELAY))
-	fi
-
-	# send regular message
-	echo "Sending Message from RialtoParachain to Millau"
-	SEND_MESSAGE_OUTPUT=`$SEND_MESSAGE --lane $MESSAGE_LANE --use-xcm-pallet $CONVERSION_RATE_OVERRIDE raw 020419ac 2>&1`
-	echo $SEND_MESSAGE_OUTPUT
-	if [ "$CONVERSION_RATE_OVERRIDE" = "--conversion-rate-override metric" ]; then
-		ACTUAL_CONVERSION_RATE_REGEX="conversion rate override: ([0-9\.]+)"
-		if [[ $SEND_MESSAGE_OUTPUT =~ $ACTUAL_CONVERSION_RATE_REGEX ]]; then
-			CONVERSION_RATE=${BASH_REMATCH[1]}
-			CONVERSION_RATE_OVERRIDE="--conversion-rate-override $CONVERSION_RATE"
-		else
-			echo "Error: unable to find conversion rate in send-message output. Will keep using on-chain rate"
-			CONVERSION_RATE_OVERRIDE=""
-		fi
-	fi
-
-	# every other hour we're sending large message
-	if [ $SECONDS -ge $LARGE_MESSAGES_TIME ]; then
-		LARGE_MESSAGES_TIME=$((SECONDS + 7200))
-
-		rand_sleep
-		echo "Sending Maximal Size Message from RialtoParachain to Millau"
-		$SEND_MESSAGE \
-			--lane $MESSAGE_LANE \
-			$CONVERSION_RATE_OVERRIDE \
-			sized max
-	fi
-
-	# every other hour we're sending a bunch of small messages
-	if [ $SECONDS -ge $BUNCH_OF_MESSAGES_TIME ]; then
-		BUNCH_OF_MESSAGES_TIME=$((SECONDS + 7200))
-		for i in $(seq 0 $MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE);
-		do
-			$SEND_MESSAGE \
-				--lane $MESSAGE_LANE \
-				--use-xcm-pallet \
-				$CONVERSION_RATE_OVERRIDE \
-				raw 010109030419A8
-		done
-	fi
-done
+source /common/generate_messages.sh
