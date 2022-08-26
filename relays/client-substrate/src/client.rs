7268261ed46befab3b5630be31b07d6bf891ddb9
Branislav Kontur <bkontur@gmail.com>
2022-08-26 16:38:59 +0200

    Client - ConnectionParams wrapped with Arc + removed unnecessery clone

--- relays/client-substrate/src/client.rs
+++ relays/client-substrate/src/client.rs
@@ -131,6 +131,7 @@ impl<C: Chain> Client<C> {
 	/// This function will keep connecting to given Substrate node until connection is established
 	/// and is functional. If attempt fail, it will wait for `RECONNECT_DELAY` and retry again.
 	pub async fn new(params: ConnectionParams) -> Self {
+		let params = Arc::new(params);
 		loop {
 			match Self::try_connect(params.clone()).await {
 				Ok(client) => return client,
@@ -149,7 +150,7 @@ impl<C: Chain> Client<C> {
 
 	/// Try to connect to Substrate node over websocket. Returns Substrate RPC client if connection
 	/// has been established or error otherwise.
-	pub async fn try_connect(params: ConnectionParams) -> Result<Self> {
+	pub async fn try_connect(params: Arc<ConnectionParams>) -> Result<Self> {
 		let (tokio, client) = Self::build_client(&params).await?;
 
 		let number: C::BlockNumber = Zero::zero();
@@ -163,7 +164,7 @@ impl<C: Chain> Client<C> {
 		let chain_runtime_version = params.chain_runtime_version.clone();
 		Ok(Self {
 			tokio,
-			params: Arc::new(params),
+			params,
 			client,
 			genesis_hash,
 			submit_signed_extrinsic_lock: Arc::new(Mutex::new(())),
