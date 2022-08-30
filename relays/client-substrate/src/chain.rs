e7502c3295cad1a8c12a7295087802eff6069bc2
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-08-30 14:31:35 +0300

    substrate-relay is compiling

--- relays/client-substrate/src/chain.rs
+++ relays/client-substrate/src/chain.rs
@@ -17,7 +17,7 @@
 use bp_messages::MessageNonce;
 use bp_runtime::{Chain as ChainBase, EncodedOrDecodedCall, HashOf, TransactionEraOf};
 use codec::{Codec, Encode};
-use frame_support::weights::{Weight, WeightToFeePolynomial};
+use frame_support::weights::{Weight, WeightToFee};
 use jsonrpsee::core::{DeserializeOwned, Serialize};
 use num_traits::Zero;
 use sc_transaction_pool_api::TransactionStatus;
@@ -59,7 +59,7 @@ pub trait Chain: ChainBase + Clone {
 	type Call: Clone + Codec + Dispatchable + Debug + Send;
 
 	/// Type that is used by the chain, to convert from weight to fee.
-	type WeightToFee: WeightToFeePolynomial<Balance = Self::Balance>;
+	type WeightToFee: WeightToFee<Balance = Self::Balance>;
 }
 
 /// Substrate-based relay chain that supports parachains.
