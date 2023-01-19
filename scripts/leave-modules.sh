94acb7bfbae37656bb88a354b95e3ca943243a73
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-01-19 12:46:02 +0300

    cleanup script

--- scripts/leave-modules.sh
+++ scripts/leave-modules.sh
@@ -72,7 +72,3 @@ cargo check -p pallet-bridge-relayers
 cargo check -p pallet-bridge-relayers --features runtime-benchmarks
 cargo check -p bridge-runtime-common
 cargo check -p bridge-runtime-common --features runtime-benchmarks
-
-# everything is ok - we may not clean the 
-
-#trap - EXIT
