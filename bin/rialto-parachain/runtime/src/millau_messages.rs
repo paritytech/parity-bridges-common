a9b1b8f572965d136b5f12dc3cc8dc5a6352c657
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-05-01 11:29:44 +0300

    post cherry-pick fixes

--- bin/rialto-parachain/runtime/src/millau_messages.rs
+++ bin/rialto-parachain/runtime/src/millau_messages.rs
@@ -154,7 +154,6 @@ mod tests {
 			bp_millau::EXTRA_STORAGE_PROOF_SIZE,
 			bp_rialto_parachain::MAX_UNREWARDED_RELAYERS_IN_CONFIRMATION_TX,
 			bp_rialto_parachain::MAX_UNCONFIRMED_MESSAGES_IN_CONFIRMATION_TX,
-			false,
 		);
 	}
 
