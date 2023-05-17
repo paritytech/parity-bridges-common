2ad287b8d90967b205469ab7d1d89ac7c4a8b5f2
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-05-17 10:49:28 +0300

    ws-port argument has been repalced with rpc-port

--- deployments/local-scripts/run-rialto-node.sh
+++ deployments/local-scripts/run-rialto-node.sh
@@ -8,4 +8,4 @@ RIALTO_PORT="${RIALTO_PORT:-9944}"
 RUST_LOG=runtime=trace \
     ./target/debug/rialto-bridge-node --dev --tmp \
     --rpc-cors=all --unsafe-rpc-external \
-    --port 33033 --rpc-port 9933 --ws-port $RIALTO_PORT \
+    --port 33033 --rpc-port $RIALTO_PORT \
