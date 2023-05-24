086a5ced8931b21799721de3f86bdc067e9282eb
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-05-24 14:01:03 +0300

    local.Dockerfile for building images from locally built binaries (#2154)
    * local.Dockerfile for building images from locally built binaries
    
    * fix doc
    
    * moved compatibility comment
    
    * 1000 -> 1001

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 set -eux
 
-time docker build . -t local/substrate-relay --build-arg=PROJECT=substrate-relay
-time docker build . -t local/rialto-bridge-node --build-arg=PROJECT=rialto-bridge-node
-time docker build . -t local/millau-bridge-node --build-arg=PROJECT=millau-bridge-node
-time docker build . -t local/rialto-parachain-collator --build-arg=PROJECT=rialto-parachain-collator
+if [ -z "${LOCAL:-}" ]; then
+    time docker build . -t local/substrate-relay --build-arg=PROJECT=substrate-relay
+    time docker build . -t local/rialto-bridge-node --build-arg=PROJECT=rialto-bridge-node
+    time docker build . -t local/millau-bridge-node --build-arg=PROJECT=millau-bridge-node
+    time docker build . -t local/rialto-parachain-collator --build-arg=PROJECT=rialto-parachain-collator
+else
+    if [ -z "${SKIP_BUILD:-}" ]; then
+        time cargo build -p substrate-relay --release
+        time cargo build -p rialto-bridge-node --release
+        time cargo build -p millau-bridge-node --release
+        time cargo build -p rialto-parachain-collator --release
+    fi
+
+    # following (using DOCKER_BUILDKIT) requires docker 19.03 or above
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/substrate-relay --build-arg=PROJECT=substrate-relay
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/rialto-bridge-node --build-arg=PROJECT=rialto-bridge-node
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/millau-bridge-node --build-arg=PROJECT=millau-bridge-node
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/rialto-parachain-collator --build-arg=PROJECT=rialto-parachain-collator
+fi
