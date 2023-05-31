0d279f174149798898d513281622873d198fe95b
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-05-31 13:51:22 +0300

    enum for runtime calls

--- relays/client-substrate/src/client/runtime_call.rs
+++ relays/client-substrate/src/client/runtime_call.rs
@@ -26,24 +26,65 @@
 // TODO: rename `runtime_call.rs` and `RuntimeCall` associated type to avoid confusions
 // with actual `RuntimeCall` enum???!!!
 
-use crate::{Chain, HeaderOf, SyncHeader};
+use crate::{AccountIdOf, Chain, HeaderOf, SyncHeader};
 use bp_header_chain::justification::GrandpaJustification;
-use frame_support::DebugNoBound;
+use bp_polkadot_core::parachains::ParaHeadsProof;
+use frame_support::{weights::Weight, DebugNoBound};
 
-/// Runtime call that brings finalized header of the bridged chain.
+/// Intermediate message proof returned by the source Substrate node. Includes everything
+/// required to submit to the target node: cumulative dispatch weight of bundled messages and
+/// the proof itself.
+pub type SubstrateMessagesProof<C> = (Weight, FromBridgedChainMessagesProof<HashOf<C>>);
+
+/// Message receiving proof returned by the target Substrate node.
+pub type SubstrateMessagesDeliveryProof<C> =
+	(UnrewardedRelayersState, FromBridgedChainMessagesDeliveryProof<HashOf<C>>);
+
+/// All runtime calls that are supported by the messages and finality relays.
 #[derive(Clone, DebugNoBound)]
-pub struct GrandpaFinalityCall<BridgedChain: Chain> {
+pub enum RuntimeCall<BridgedChain: Chain> {
+	/// Runtime call that brings GRANDPA-finalized header of the bridged chain.
+	GrandpaFinality(GrandpaFinalityCall<BridgedChain>),
+	/// Runtime call that brings finalized header of the bridged parachain.
+	ParachainFinality(ParachainFinalityCall<BridgedChain>),
+	/// Runtime call that brings new messages from the bridged chain.
+	MessagesDelivery(MessagesDeliveryCall<BridgedChain>),
+	/// Runtime call that brings new delivery confirmations from the bridged chain.
+	MessagesDeliveryConfirmation(MessagesDeliveryConfirmationCall<BridgedChain>),
+	/// Runtime call that brings multiple runtime calls at once.
+	BatchCall(Vec<RuntimeCall<BridgedChain>>),
+}
+
+/// Runtime call that brings GRANDPA-finalized header of the bridged chain.
+#[derive(Clone, DebugNoBound)]
+pub struct GrandpaFinalityCall<BridgedChain> {
 	/// Finalized bridged chain header.
 	pub header: SyncHeader<HeaderOf<BridgedChain>>,
 	/// Finality proof of the bridged chain header.
 	pub justification: GrandpaJustification<HeaderOf<BridgedChain>>,
 }
 
+/// Runtime call that brings finalized header of the bridged parachain.
+#[derive(Clone, DebugNoBound)]
+pub struct ParachainFinalityCall<BridgedChain> {
+	/// Finalized bridged parachain header.
+	pub header: SyncHeader<HeaderOf<BridgedChain>>,
+	/// Finality proof of the bridged chain header.
+	pub proof: ParaHeadsProof,
+}
+
 /// Runtime call that brings new messages from the bridged chain.
 #[derive(Clone, DebugNoBound)]
-pub struct MessagesDeliveryCall<BridgedChain: Chain> {
+pub struct MessagesDeliveryCall<BridgedChain> {
 	/// Relayer account at the bridged chain.
 	pub relayer_id_at_source: AccountIdOf<BridgedChain>,
 	/// Bundled messages proof.
 	pub proof: SubstrateMessagesProof<BridgedChain>,
+}
+
+/// Runtime call that brings new delivery confirmations from the bridged chain.
+#[derive(Clone, DebugNoBound)]
+pub struct MessagesDeliveryConfirmationCall<BridgedChain> {
+	/// Bundled messages proof.
+	pub proof: SubstrateMessagesDeliveryProof<BridgedChain>,
 }
