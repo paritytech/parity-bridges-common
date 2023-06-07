18aabff1b7a91e0006e1281668096a4eeaa83f2a
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-06-07 13:55:52 +0300

    reuse messages proof generation from messages pallet

--- bin/runtime-common/src/messages_benchmarking.rs
+++ bin/runtime-common/src/messages_benchmarking.rs
@@ -19,30 +19,24 @@
 
 #![cfg(feature = "runtime-benchmarks")]
 
-// TODO: https://github.com/paritytech/parity-bridges-common/issues/1666 - merge with message
-// generation methods from messages pallet and use it here
-
 use bp_messages::{
-	source_chain::FromBridgedChainMessagesDeliveryProof, storage_keys,
-	target_chain::FromBridgedChainMessagesProof, ChainWithMessages,
+	source_chain::FromBridgedChainMessagesDeliveryProof,
+	target_chain::FromBridgedChainMessagesProof,
 };
 use bp_polkadot_core::parachains::ParaHash;
-use bp_runtime::{
-	grow_trie_leaf_value, record_all_trie_keys, AccountIdOf, Chain, HashOf, HasherOf, Parachain,
-	StorageProofSize, UntrustedVecDb,
-};
+use bp_runtime::{AccountIdOf, Chain, HashOf, Parachain, StorageProofSize};
 use codec::Encode;
-use frame_support::{weights::Weight, StateVersion};
+use frame_support::weights::Weight;
 use pallet_bridge_messages::{
 	benchmarking::{MessageDeliveryProofParams, MessageProofParams},
-	messages_generation::{encode_all_messages, encode_lane_data, prepare_messages_storage_proof},
+	messages_generation::{
+		encode_all_messages, encode_lane_data, prepare_message_delivery_storage_proof,
+		prepare_messages_storage_proof,
+	},
 	BridgedChainOf, ThisChainOf,
 };
 use sp_runtime::traits::{Header, Zero};
 use sp_std::prelude::*;
-use sp_trie::{
-	LayoutV0, LayoutV1, MemoryDB, StorageProof, TrieConfiguration, TrieDBMutBuilder, TrieMut,
-};
 use xcm::v3::prelude::*;
 
 /// Prepare inbound bridge message according to given message proof parameters.
@@ -194,7 +188,10 @@ where
 {
 	// prepare storage proof
 	let lane = params.lane;
-	let (state_root, storage_proof) = prepare_message_delivery_proof::<R, MI>(params);
+	let (state_root, storage_proof) = prepare_message_delivery_storage_proof::<
+		BridgedChainOf<R, MI>,
+		ThisChainOf<R, MI>,
+	>(params.lane, params.inbound_lane_data, params.size);
 
 	// update runtime storage
 	let (_, bridged_header_hash) = insert_header_to_grandpa_pallet::<R, FI>(state_root);
@@ -222,7 +219,10 @@ where
 {
 	// prepare storage proof
 	let lane = params.lane;
-	let (state_root, storage_proof) = prepare_message_delivery_proof::<R, MI>(params);
+	let (state_root, storage_proof) = prepare_message_delivery_storage_proof::<
+		BridgedChainOf<R, MI>,
+		ThisChainOf<R, MI>,
+	>(params.lane, params.inbound_lane_data, params.size);
 
 	// update runtime storage
 	let (_, bridged_header_hash) =
@@ -233,69 +233,6 @@ where
 		storage_proof,
 		lane,
 	}
-}
-
-/// Prepare in-memory message delivery proof, without inserting anything to the runtime storage.
-fn prepare_message_delivery_proof<R, MI>(
-	params: MessageDeliveryProofParams<AccountIdOf<ThisChainOf<R, MI>>>,
-) -> (HashOf<BridgedChainOf<R, MI>>, UntrustedVecDb)
-where
-	R: pallet_bridge_messages::Config<MI>,
-	MI: 'static,
-{
-	match BridgedChainOf::<R, MI>::STATE_VERSION {
-		StateVersion::V0 =>
-			do_prepare_message_delivery_proof::<R, MI, LayoutV0<HasherOf<BridgedChainOf<R, MI>>>>(
-				params,
-			),
-		StateVersion::V1 =>
-			do_prepare_message_delivery_proof::<R, MI, LayoutV1<HasherOf<BridgedChainOf<R, MI>>>>(
-				params,
-			),
-	}
-}
-
-/// Prepare in-memory message delivery proof, without inserting anything to the runtime storage.
-fn do_prepare_message_delivery_proof<
-	R,
-	MI,
-	L: TrieConfiguration<Hash = HasherOf<BridgedChainOf<R, MI>>>,
->(
-	params: MessageDeliveryProofParams<AccountIdOf<ThisChainOf<R, MI>>>,
-) -> (HashOf<BridgedChainOf<R, MI>>, UntrustedVecDb)
-where
-	R: pallet_bridge_messages::Config<MI>,
-	MI: 'static,
-{
-	// prepare Bridged chain storage with inbound lane state
-	let storage_key = storage_keys::inbound_lane_data_key(
-		R::ThisChain::WITH_CHAIN_MESSAGES_PALLET_NAME,
-		&params.lane,
-	)
-	.0;
-	let mut root = Default::default();
-	let mut mdb = MemoryDB::default();
-	{
-		let mut trie = TrieDBMutBuilder::<L>::new(&mut mdb, &mut root).build();
-		let inbound_lane_data =
-			grow_trie_leaf_value(params.inbound_lane_data.encode(), params.size);
-		trie.insert(&storage_key, &inbound_lane_data)
-			.map_err(|_| "TrieMut::insert has failed")
-			.expect("TrieMut::insert should not fail in benchmarks");
-	}
-
-	// generate storage proof to be delivered to This chain
-	let read_proof = record_all_trie_keys::<L, _>(&mdb, &root)
-		.map_err(|_| "record_all_trie_keys has failed")
-		.expect("record_all_trie_keys should not fail in benchmarks");
-	let storage_proof = UntrustedVecDb::try_new::<HasherOf<BridgedChainOf<R, MI>>>(
-		StorageProof::new(read_proof),
-		root,
-		vec![storage_key],
-	)
-	.unwrap();
-
-	(root, storage_proof)
 }
 
 /// Insert header to the bridge GRANDPA pallet.
