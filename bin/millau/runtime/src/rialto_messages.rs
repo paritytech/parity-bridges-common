8f3f594b4e78eda87c865ad4b87c0ba80275b062
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-06-07 15:17:04 +0300

    fixed remaining TODOs after messages pallet Config refactoring

--- bin/millau/runtime/src/rialto_messages.rs
+++ bin/millau/runtime/src/rialto_messages.rs
@@ -85,7 +85,7 @@ mod tests {
 	use bridge_runtime_common::{
 		assert_complete_bridge_types,
 		integrity::{
-			assert_complete_bridge_constants, check_message_lane_weights, AssertBridgePalletNames,
+			assert_complete_with_relay_chain_bridge_constants, check_message_lane_weights,
 			AssertChainConstants, AssertCompleteBridgeConstants,
 		},
 	};
@@ -110,7 +110,7 @@ mod tests {
 			bridged_chain: bp_rialto::Rialto,
 		);
 
-		assert_complete_bridge_constants::<
+		assert_complete_with_relay_chain_bridge_constants::<
 			Runtime,
 			RialtoGrandpaInstance,
 			WithRialtoMessagesInstance,
@@ -118,12 +118,6 @@ mod tests {
 			this_chain_constants: AssertChainConstants {
 				block_length: bp_millau::BlockLength::get(),
 				block_weights: bp_millau::BlockWeights::get(),
-			},
-			pallet_names: AssertBridgePalletNames {
-				with_this_chain_messages_pallet_name: bp_millau::WITH_MILLAU_MESSAGES_PALLET_NAME,
-				with_bridged_chain_grandpa_pallet_name: bp_rialto::WITH_RIALTO_GRANDPA_PALLET_NAME,
-				with_bridged_chain_messages_pallet_name:
-					bp_rialto::WITH_RIALTO_MESSAGES_PALLET_NAME,
 			},
 		});
 	}
--- bin/millau/runtime/src/rialto_parachain_messages.rs
+++ bin/millau/runtime/src/rialto_parachain_messages.rs
@@ -91,7 +91,7 @@ mod tests {
 	use bridge_runtime_common::{
 		assert_complete_bridge_types,
 		integrity::{
-			assert_complete_bridge_constants, check_message_lane_weights, AssertBridgePalletNames,
+			assert_complete_with_parachain_bridge_constants, check_message_lane_weights,
 			AssertChainConstants, AssertCompleteBridgeConstants,
 		},
 	};
@@ -113,23 +113,18 @@ mod tests {
 			with_bridged_chain_grandpa_instance: RialtoGrandpaInstance,
 			with_bridged_chain_messages_instance: WithRialtoParachainMessagesInstance,
 			this_chain: bp_millau::Millau,
-			bridged_chain: bp_rialto::Rialto,
+			bridged_chain: bp_rialto_parachain::RialtoParachain,
 		);
 
-		assert_complete_bridge_constants::<
+		assert_complete_with_parachain_bridge_constants::<
 			Runtime,
 			RialtoGrandpaInstance,
 			WithRialtoParachainMessagesInstance,
+			bp_rialto::Rialto,
 		>(AssertCompleteBridgeConstants {
 			this_chain_constants: AssertChainConstants {
 				block_length: bp_millau::BlockLength::get(),
 				block_weights: bp_millau::BlockWeights::get(),
-			},
-			pallet_names: AssertBridgePalletNames {
-				with_this_chain_messages_pallet_name: bp_millau::WITH_MILLAU_MESSAGES_PALLET_NAME,
-				with_bridged_chain_grandpa_pallet_name: bp_rialto::WITH_RIALTO_GRANDPA_PALLET_NAME,
-				with_bridged_chain_messages_pallet_name:
-					bp_rialto_parachain::WITH_RIALTO_PARACHAIN_MESSAGES_PALLET_NAME,
 			},
 		});
 
