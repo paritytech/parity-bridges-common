167548890225e8265a619237f4ebcd814ee187ff
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-06-27 13:40:26 +0300

    use host Ubuntu version to build images with prebuilt binaries (#2232)

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -11,9 +11,20 @@ else
         time cargo build -p substrate-relay -p rialto-bridge-node -p millau-bridge-node -p rialto-parachain-collator --release
     fi
 
+    # (try to) use docker image matching the host os
+    export UBUNTU_RELEASE=`lsb_release -r -s`
+
     # following (using DOCKER_BUILDKIT) requires docker 19.03 or above
-    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/substrate-relay --build-arg=PROJECT=substrate-relay
-    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/rialto-bridge-node --build-arg=PROJECT=rialto-bridge-node
-    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/millau-bridge-node --build-arg=PROJECT=millau-bridge-node
-    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/rialto-parachain-collator --build-arg=PROJECT=rialto-parachain-collator
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/substrate-relay \
+        --build-arg=PROJECT=substrate-relay \
+        --build-arg=UBUNTU_RELEASE=${UBUNTU_RELEASE}
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/rialto-bridge-node \
+        --build-arg=PROJECT=rialto-bridge-node \
+        --build-arg=UBUNTU_RELEASE=${UBUNTU_RELEASE}
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/millau-bridge-node \
+        --build-arg=PROJECT=millau-bridge-node \
+        --build-arg=UBUNTU_RELEASE=${UBUNTU_RELEASE}
+    DOCKER_BUILDKIT=1 time docker build . -f local.Dockerfile -t local/rialto-parachain-collator \
+        --build-arg=PROJECT=rialto-parachain-collator \
+        --build-arg=UBUNTU_RELEASE=${UBUNTU_RELEASE}
 fi
