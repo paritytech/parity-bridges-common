0298cf94509177ce18119e9862f299cb18afc47c
Dastan <dastan@freeverse.io>
2023-08-24 14:17:05 +0300

    Update evochain types

--- primitives/chain-evolution/src/lib.rs
+++ primitives/chain-evolution/src/lib.rs
@@ -38,7 +38,7 @@ use serde::{Deserialize, Serialize};
 use sp_core::{storage::StateVersion, Hasher as HasherT};
 use sp_runtime::{
 	traits::{BlakeTwo256, IdentifyAccount, Verify},
-	MultiSigner, Perbill,
+	MultiSigner, Perbill, MultiSignature,
 };
 use sp_std::prelude::*;
 
@@ -136,7 +136,7 @@ pub type Hasher = BlakeTwo256;
 pub type Header = sp_runtime::generic::Header<BlockNumber, Hasher>;
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
-pub type Signature = fp_account::EthereumSignature;
+pub type Signature = MultiSignature;
 
 /// Some way of identifying an account on the chain. We intentionally make it equivalent
 /// to the public key of our transaction signing scheme.
