3b4f26e37487bc220b69509d3ba0f73c08af9a12
Branislav Kontur <bkontur@gmail.com>
2023-10-19 13:15:30 +0200

    Backport from `polkadot-sdk` with actual master (#2633) (#2638)
    * Backport from `polkadot-sdk`
    
    * lingua stuff
    
    * Fixes
    
    * Fix
    
    * Features?
    
    * Fix - v5 -> v6
    
    * zepter format features
    
    * try zepter with CI
    
    * Fix imports
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Revert zepther pipeline

--- relays/bin-substrate/src/cli/register_parachain.rs
+++ relays/bin-substrate/src/cli/register_parachain.rs
@@ -104,8 +104,8 @@ impl RegisterParachain {
 			let para_id: ParaId = relay_client
 				.storage_value(best_relay_header_hash, StorageKey(para_id_key.to_vec()))
 				.await?
-				.unwrap_or(polkadot_primitives::v5::LOWEST_PUBLIC_ID)
-				.max(polkadot_primitives::v5::LOWEST_PUBLIC_ID);
+				.unwrap_or(polkadot_primitives::v6::LOWEST_PUBLIC_ID)
+				.max(polkadot_primitives::v6::LOWEST_PUBLIC_ID);
 			log::info!(target: "bridge", "Going to reserve parachain id: {:?}", para_id);
 
 			// step 1: reserve a parachain id
