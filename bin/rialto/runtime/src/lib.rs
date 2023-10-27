a4acfe4057417af2296d6e12a83bd7fce366aced
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-10-27 10:26:59 +0300

    Introduce ActiveLaneRelayersSet and NextLaneRelayersSet (#2627)
    * intreoduce ActiveLaneRelayersSet and NextLaneRelayersSet
    
    * more tests
    
    * fix runtimes
    
    * CI
    
    * CI 2
    
    * CI 3
    
    * apply review suggestions
    
    * return Option<> from try_remove
    
    * RelayerAndReward -> LaneRegistration
    
    * separate BTreeSet -> boolean flag

--- bin/rialto/runtime/src/lib.rs
+++ bin/rialto/runtime/src/lib.rs
@@ -397,7 +397,8 @@ impl pallet_bridge_relayers::Config for Runtime {
 	type PaymentProcedure =
 		bp_relayers::PayRewardFromAccount<pallet_balances::Pallet<Runtime>, AccountId>;
 	type StakeAndSlash = ();
-	type MaxRelayersPerLane = ConstU32<16>;
+	type MaxActiveRelayersPerLane = ConstU32<16>;
+	type MaxNextRelayersPerLane = ConstU32<1_024>;
 	type SlotLength = ConstU32<16>;
 	type PriorityBoostPerMessage = ConstU64<0>;
 	type PriorityBoostForActiveLaneRelayer = ConstU64<0>;
