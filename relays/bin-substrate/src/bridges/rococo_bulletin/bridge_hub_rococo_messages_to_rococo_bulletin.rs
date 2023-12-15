paritytech/polkadot-sdk

Added Rococo BH <> Rococo Bulletin bridge (#2724)
* added Rococo BH <> Rococo Bulletin bridge

* init-bridge support

* allow customising finality-related runtime APIs

* revert me

* use Rococo/BridgeHubRococo pretending to be a Polkadot/BridgeHubPolkadot in Rococo <> RococoBulletin bridge

* Revert "revert me"

This reverts commit 90c598d9d50a25e7182c97eee7818bf8d4bc404c.

* Revert "allow customising finality-related runtime APIs"

This reverts commit b39c32c34acddfd0b919042122e0e667470bd0a4.

* fmt

* WITH_BRIDGE_ROCOCO_TO_BULLETIN_MESSAGES_PALLET_INDEX

* regenerate bulletin chain runtime (pallet indices have changed)

* fx WITH_BRIDGE_ROCOCO_TO_BULLETIN_MESSAGES_PALLET_INDEX constant because of latest changes

* also change indices in runtime

* fmt

* clippy

relays/bin-substrate/src/bridges/rococo_bulletin/bridge_hub_rococo_messages_to_rococo_bulletin.rs
@@ -1,0 +1,65 @@
+// Copyright 2022 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! BridgeHubRococo-to-RococoBulletin messages sync entrypoint.
+
+use super::BridgeHubRococoAsBridgeHubPolkadot;
+use crate::cli::bridge::{CliBridgeBase, MessagesCliBridge};
+use relay_polkadot_bulletin_client::PolkadotBulletin as RococoBulletin;
+use substrate_relay_helper::{messages_lane::SubstrateMessageLane, UtilityPalletBatchCallBuilder};
+
+/// BridgeHubRococo-to-RococoBulletin messages bridge.
+pub struct BridgeHubRococoToRococoBulletinMessagesCliBridge {}
+
+impl CliBridgeBase for BridgeHubRococoToRococoBulletinMessagesCliBridge {
+	type Source = BridgeHubRococoAsBridgeHubPolkadot;
+	type Target = RococoBulletin;
+}
+
+impl MessagesCliBridge for BridgeHubRococoToRococoBulletinMessagesCliBridge {
+	type MessagesLane = BridgeHubRococoMessagesToRococoBulletinMessageLane;
+}
+
+substrate_relay_helper::generate_receive_message_proof_call_builder!(
+	BridgeHubRococoMessagesToRococoBulletinMessageLane,
+	BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesProofCallBuilder,
+	relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotMessages,
+	relay_polkadot_bulletin_client::BridgePolkadotMessagesCall::receive_messages_proof
+);
+
+substrate_relay_helper::generate_receive_message_delivery_proof_call_builder!(
+	BridgeHubRococoMessagesToRococoBulletinMessageLane,
+	BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesDeliveryProofCallBuilder,
+	relay_bridge_hub_rococo_client::RuntimeCall::BridgePolkadotBulletinMessages,
+	relay_bridge_hub_rococo_client::BridgeBulletinMessagesCall::receive_messages_delivery_proof
+);
+
+/// BridgeHubRococo-to-RococoBulletin messages lane.
+#[derive(Clone, Debug)]
+pub struct BridgeHubRococoMessagesToRococoBulletinMessageLane;
+
+impl SubstrateMessageLane for BridgeHubRococoMessagesToRococoBulletinMessageLane {
+	type SourceChain = BridgeHubRococoAsBridgeHubPolkadot;
+	type TargetChain = RococoBulletin;
+
+	type ReceiveMessagesProofCallBuilder =
+		BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesProofCallBuilder;
+	type ReceiveMessagesDeliveryProofCallBuilder =
+		BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesDeliveryProofCallBuilder;
+
+	type SourceBatchCallBuilder = UtilityPalletBatchCallBuilder<BridgeHubRococoAsBridgeHubPolkadot>;
+	type TargetBatchCallBuilder = ();
+}

relays/bin-substrate/src/bridges/rococo_bulletin/rococo_bulletin_messages_to_bridge_hub_rococo.rs
@@ -1,0 +1,65 @@
+// Copyright 2022 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! RococoBulletin-to-BridgeHubRococo messages sync entrypoint.
+
+use super::BridgeHubRococoAsBridgeHubPolkadot;
+use crate::cli::bridge::{CliBridgeBase, MessagesCliBridge};
+use relay_polkadot_bulletin_client::PolkadotBulletin as RococoBulletin;
+use substrate_relay_helper::{messages_lane::SubstrateMessageLane, UtilityPalletBatchCallBuilder};
+
+/// RococoBulletin-to-BridgeHubRococo messages bridge.
+pub struct RococoBulletinToBridgeHubRococoMessagesCliBridge {}
+
+impl CliBridgeBase for RococoBulletinToBridgeHubRococoMessagesCliBridge {
+	type Source = RococoBulletin;
+	type Target = BridgeHubRococoAsBridgeHubPolkadot;
+}
+
+impl MessagesCliBridge for RococoBulletinToBridgeHubRococoMessagesCliBridge {
+	type MessagesLane = RococoBulletinMessagesToBridgeHubRococoMessageLane;
+}
+
+substrate_relay_helper::generate_receive_message_proof_call_builder!(
+	RococoBulletinMessagesToBridgeHubRococoMessageLane,
+	RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesProofCallBuilder,
+	relay_bridge_hub_rococo_client::RuntimeCall::BridgePolkadotBulletinMessages,
+	relay_bridge_hub_rococo_client::BridgeBulletinMessagesCall::receive_messages_proof
+);
+
+substrate_relay_helper::generate_receive_message_delivery_proof_call_builder!(
+	RococoBulletinMessagesToBridgeHubRococoMessageLane,
+	RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesDeliveryProofCallBuilder,
+	relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotMessages,
+	relay_polkadot_bulletin_client::BridgePolkadotMessagesCall::receive_messages_delivery_proof
+);
+
+/// RococoBulletin-to-BridgeHubRococo messages lane.
+#[derive(Clone, Debug)]
+pub struct RococoBulletinMessagesToBridgeHubRococoMessageLane;
+
+impl SubstrateMessageLane for RococoBulletinMessagesToBridgeHubRococoMessageLane {
+	type SourceChain = RococoBulletin;
+	type TargetChain = BridgeHubRococoAsBridgeHubPolkadot;
+
+	type ReceiveMessagesProofCallBuilder =
+		RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesProofCallBuilder;
+	type ReceiveMessagesDeliveryProofCallBuilder =
+		RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesDeliveryProofCallBuilder;
+
+	type SourceBatchCallBuilder = ();
+	type TargetBatchCallBuilder = UtilityPalletBatchCallBuilder<BridgeHubRococoAsBridgeHubPolkadot>;
+}

relays/bin-substrate/src/bridges/rococo_bulletin/rococo_parachains_to_rococo_bulletin.rs
@@ -1,0 +1,91 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Rococo-to-RococoBulletin parachains sync entrypoint.
+
+use super::{BridgeHubRococoAsBridgeHubPolkadot, RococoAsPolkadot};
+use crate::cli::bridge::{CliBridgeBase, MessagesCliBridge, ParachainToRelayHeadersCliBridge};
+
+use bp_polkadot_core::parachains::{ParaHash, ParaHeadsProof, ParaId};
+use bp_runtime::Chain;
+use relay_substrate_client::{CallOf, HeaderIdOf};
+use substrate_relay_helper::{
+	messages_lane::MessagesRelayLimits,
+	parachains::{SubmitParachainHeadsCallBuilder, SubstrateParachainsPipeline},
+};
+
+/// Rococo-to-RococoBulletin parachain sync description.
+#[derive(Clone, Debug)]
+pub struct RococoToRococoBulletin;
+
+impl SubstrateParachainsPipeline for RococoToRococoBulletin {
+	type SourceParachain = BridgeHubRococoAsBridgeHubPolkadot;
+	type SourceRelayChain = RococoAsPolkadot;
+	type TargetChain = relay_polkadot_bulletin_client::PolkadotBulletin;
+
+	type SubmitParachainHeadsCallBuilder = RococoToRococoBulletinCallBuilder;
+}
+
+pub struct RococoToRococoBulletinCallBuilder;
+impl SubmitParachainHeadsCallBuilder<RococoToRococoBulletin> for RococoToRococoBulletinCallBuilder {
+	fn build_submit_parachain_heads_call(
+		at_relay_block: HeaderIdOf<relay_rococo_client::Rococo>,
+		parachains: Vec<(ParaId, ParaHash)>,
+		parachain_heads_proof: ParaHeadsProof,
+	) -> CallOf<relay_polkadot_bulletin_client::PolkadotBulletin> {
+		relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotParachains(
+			relay_polkadot_bulletin_client::BridgePolkadotParachainsCall::submit_parachain_heads {
+				at_relay_block: (at_relay_block.0, at_relay_block.1),
+				parachains,
+				parachain_heads_proof,
+			},
+		)
+	}
+}
+
+/// Rococo-to-RococoBulletin parachain sync description for the CLI.
+pub struct RococoToRococoBulletinCliBridge {}
+
+impl ParachainToRelayHeadersCliBridge for RococoToRococoBulletinCliBridge {
+	type SourceRelay = RococoAsPolkadot;
+	type ParachainFinality = RococoToRococoBulletin;
+	type RelayFinality =
+		crate::bridges::rococo_bulletin::rococo_headers_to_rococo_bulletin::RococoFinalityToRococoBulletin;
+}
+
+impl CliBridgeBase for RococoToRococoBulletinCliBridge {
+	type Source = BridgeHubRococoAsBridgeHubPolkadot;
+	type Target = relay_polkadot_bulletin_client::PolkadotBulletin;
+}
+
+impl MessagesCliBridge for RococoToRococoBulletinCliBridge {
+	type MessagesLane =
+		crate::bridges::rococo_bulletin::bridge_hub_rococo_messages_to_rococo_bulletin::BridgeHubRococoMessagesToRococoBulletinMessageLane;
+
+	fn maybe_messages_limits() -> Option<MessagesRelayLimits> {
+		// Rococo Bulletin chain is missing the `TransactionPayment` runtime API (as well as the
+		// transaction payment pallet itself), so we can't estimate limits using runtime calls.
+		// Let's do it here.
+		//
+		// Folloiung constants are just safe **underestimations**. Normally, we are able to deliver
+		// and dispatch thousands of messages in the same transaction.
+		Some(MessagesRelayLimits {
+			max_messages_in_single_batch: 128,
+			max_messages_weight_in_single_batch:
+				bp_polkadot_bulletin::PolkadotBulletin::max_extrinsic_weight() / 20,
+		})
+	}
+}

paritytech/parity-bridges-common

relays/bin-substrate/src/bridges/rococo_bulletin/bridge_hub_rococo_messages_to_rococo_bulletin.rs
@@ -1,0 +1,65 @@
+// Copyright 2022 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! BridgeHubRococo-to-RococoBulletin messages sync entrypoint.
+
+use super::BridgeHubRococoAsBridgeHubPolkadot;
+use crate::cli::bridge::{CliBridgeBase, MessagesCliBridge};
+use relay_polkadot_bulletin_client::PolkadotBulletin as RococoBulletin;
+use substrate_relay_helper::{messages::SubstrateMessageLane, UtilityPalletBatchCallBuilder};
+
+/// BridgeHubRococo-to-RococoBulletin messages bridge.
+pub struct BridgeHubRococoToRococoBulletinMessagesCliBridge {}
+
+impl CliBridgeBase for BridgeHubRococoToRococoBulletinMessagesCliBridge {
+	type Source = BridgeHubRococoAsBridgeHubPolkadot;
+	type Target = RococoBulletin;
+}
+
+impl MessagesCliBridge for BridgeHubRococoToRococoBulletinMessagesCliBridge {
+	type MessagesLane = BridgeHubRococoMessagesToRococoBulletinMessageLane;
+}
+
+substrate_relay_helper::generate_receive_message_proof_call_builder!(
+	BridgeHubRococoMessagesToRococoBulletinMessageLane,
+	BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesProofCallBuilder,
+	relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotMessages,
+	relay_polkadot_bulletin_client::BridgePolkadotMessagesCall::receive_messages_proof
+);
+
+substrate_relay_helper::generate_receive_message_delivery_proof_call_builder!(
+	BridgeHubRococoMessagesToRococoBulletinMessageLane,
+	BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesDeliveryProofCallBuilder,
+	relay_bridge_hub_rococo_client::RuntimeCall::BridgePolkadotBulletinMessages,
+	relay_bridge_hub_rococo_client::BridgeBulletinMessagesCall::receive_messages_delivery_proof
+);
+
+/// BridgeHubRococo-to-RococoBulletin messages lane.
+#[derive(Clone, Debug)]
+pub struct BridgeHubRococoMessagesToRococoBulletinMessageLane;
+
+impl SubstrateMessageLane for BridgeHubRococoMessagesToRococoBulletinMessageLane {
+	type SourceChain = BridgeHubRococoAsBridgeHubPolkadot;
+	type TargetChain = RococoBulletin;
+
+	type ReceiveMessagesProofCallBuilder =
+		BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesProofCallBuilder;
+	type ReceiveMessagesDeliveryProofCallBuilder =
+		BridgeHubRococoMessagesToRococoBulletinMessageLaneReceiveMessagesDeliveryProofCallBuilder;
+
+	type SourceBatchCallBuilder = UtilityPalletBatchCallBuilder<BridgeHubRococoAsBridgeHubPolkadot>;
+	type TargetBatchCallBuilder = ();
+}

relays/bin-substrate/src/bridges/rococo_bulletin/rococo_bulletin_messages_to_bridge_hub_rococo.rs
@@ -1,0 +1,65 @@
+// Copyright 2022 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! RococoBulletin-to-BridgeHubRococo messages sync entrypoint.
+
+use super::BridgeHubRococoAsBridgeHubPolkadot;
+use crate::cli::bridge::{CliBridgeBase, MessagesCliBridge};
+use relay_polkadot_bulletin_client::PolkadotBulletin as RococoBulletin;
+use substrate_relay_helper::{messages::SubstrateMessageLane, UtilityPalletBatchCallBuilder};
+
+/// RococoBulletin-to-BridgeHubRococo messages bridge.
+pub struct RococoBulletinToBridgeHubRococoMessagesCliBridge {}
+
+impl CliBridgeBase for RococoBulletinToBridgeHubRococoMessagesCliBridge {
+	type Source = RococoBulletin;
+	type Target = BridgeHubRococoAsBridgeHubPolkadot;
+}
+
+impl MessagesCliBridge for RococoBulletinToBridgeHubRococoMessagesCliBridge {
+	type MessagesLane = RococoBulletinMessagesToBridgeHubRococoMessageLane;
+}
+
+substrate_relay_helper::generate_receive_message_proof_call_builder!(
+	RococoBulletinMessagesToBridgeHubRococoMessageLane,
+	RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesProofCallBuilder,
+	relay_bridge_hub_rococo_client::RuntimeCall::BridgePolkadotBulletinMessages,
+	relay_bridge_hub_rococo_client::BridgeBulletinMessagesCall::receive_messages_proof
+);
+
+substrate_relay_helper::generate_receive_message_delivery_proof_call_builder!(
+	RococoBulletinMessagesToBridgeHubRococoMessageLane,
+	RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesDeliveryProofCallBuilder,
+	relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotMessages,
+	relay_polkadot_bulletin_client::BridgePolkadotMessagesCall::receive_messages_delivery_proof
+);
+
+/// RococoBulletin-to-BridgeHubRococo messages lane.
+#[derive(Clone, Debug)]
+pub struct RococoBulletinMessagesToBridgeHubRococoMessageLane;
+
+impl SubstrateMessageLane for RococoBulletinMessagesToBridgeHubRococoMessageLane {
+	type SourceChain = RococoBulletin;
+	type TargetChain = BridgeHubRococoAsBridgeHubPolkadot;
+
+	type ReceiveMessagesProofCallBuilder =
+		RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesProofCallBuilder;
+	type ReceiveMessagesDeliveryProofCallBuilder =
+		RococoBulletinMessagesToBridgeHubRococoMessageLaneReceiveMessagesDeliveryProofCallBuilder;
+
+	type SourceBatchCallBuilder = ();
+	type TargetBatchCallBuilder = UtilityPalletBatchCallBuilder<BridgeHubRococoAsBridgeHubPolkadot>;
+}

relays/bin-substrate/src/bridges/rococo_bulletin/rococo_parachains_to_rococo_bulletin.rs
@@ -1,0 +1,91 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Rococo-to-RococoBulletin parachains sync entrypoint.
+
+use super::{BridgeHubRococoAsBridgeHubPolkadot, RococoAsPolkadot};
+use crate::cli::bridge::{CliBridgeBase, MessagesCliBridge, ParachainToRelayHeadersCliBridge};
+
+use bp_polkadot_core::parachains::{ParaHash, ParaHeadsProof, ParaId};
+use bp_runtime::Chain;
+use relay_substrate_client::{CallOf, HeaderIdOf};
+use substrate_relay_helper::{
+	messages::MessagesRelayLimits,
+	parachains::{SubmitParachainHeadsCallBuilder, SubstrateParachainsPipeline},
+};
+
+/// Rococo-to-RococoBulletin parachain sync description.
+#[derive(Clone, Debug)]
+pub struct RococoToRococoBulletin;
+
+impl SubstrateParachainsPipeline for RococoToRococoBulletin {
+	type SourceParachain = BridgeHubRococoAsBridgeHubPolkadot;
+	type SourceRelayChain = RococoAsPolkadot;
+	type TargetChain = relay_polkadot_bulletin_client::PolkadotBulletin;
+
+	type SubmitParachainHeadsCallBuilder = RococoToRococoBulletinCallBuilder;
+}
+
+pub struct RococoToRococoBulletinCallBuilder;
+impl SubmitParachainHeadsCallBuilder<RococoToRococoBulletin> for RococoToRococoBulletinCallBuilder {
+	fn build_submit_parachain_heads_call(
+		at_relay_block: HeaderIdOf<relay_rococo_client::Rococo>,
+		parachains: Vec<(ParaId, ParaHash)>,
+		parachain_heads_proof: ParaHeadsProof,
+	) -> CallOf<relay_polkadot_bulletin_client::PolkadotBulletin> {
+		relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotParachains(
+			relay_polkadot_bulletin_client::BridgePolkadotParachainsCall::submit_parachain_heads {
+				at_relay_block: (at_relay_block.0, at_relay_block.1),
+				parachains,
+				parachain_heads_proof,
+			},
+		)
+	}
+}
+
+/// Rococo-to-RococoBulletin parachain sync description for the CLI.
+pub struct RococoToRococoBulletinCliBridge {}
+
+impl ParachainToRelayHeadersCliBridge for RococoToRococoBulletinCliBridge {
+	type SourceRelay = RococoAsPolkadot;
+	type ParachainFinality = RococoToRococoBulletin;
+	type RelayFinality =
+		crate::bridges::rococo_bulletin::rococo_headers_to_rococo_bulletin::RococoFinalityToRococoBulletin;
+}
+
+impl CliBridgeBase for RococoToRococoBulletinCliBridge {
+	type Source = BridgeHubRococoAsBridgeHubPolkadot;
+	type Target = relay_polkadot_bulletin_client::PolkadotBulletin;
+}
+
+impl MessagesCliBridge for RococoToRococoBulletinCliBridge {
+	type MessagesLane =
+		crate::bridges::rococo_bulletin::bridge_hub_rococo_messages_to_rococo_bulletin::BridgeHubRococoMessagesToRococoBulletinMessageLane;
+
+	fn maybe_messages_limits() -> Option<MessagesRelayLimits> {
+		// Rococo Bulletin chain is missing the `TransactionPayment` runtime API (as well as the
+		// transaction payment pallet itself), so we can't estimate limits using runtime calls.
+		// Let's do it here.
+		//
+		// Folloiung constants are just safe **underestimations**. Normally, we are able to deliver
+		// and dispatch thousands of messages in the same transaction.
+		Some(MessagesRelayLimits {
+			max_messages_in_single_batch: 128,
+			max_messages_weight_in_single_batch:
+				bp_polkadot_bulletin::PolkadotBulletin::max_extrinsic_weight() / 20,
+		})
+	}
+}