186a73befa3bdbb1341df42093de04796b3cfbf8
Svyatoslav Nikolsky <svyatonik@gmail.com>
2024-03-13 11:32:59 +0300

    check_obsolete_submit_finality_proof may now boost transaction priority (this is not used yet - see next commit)

--- bin/runtime-common/src/extensions/refund_relayer_extension.rs
+++ bin/runtime-common/src/extensions/refund_relayer_extension.rs
@@ -689,7 +689,7 @@ where
 	fn check_obsolete_parsed_call(
 		call: &CallOf<Runtime>,
 	) -> Result<&CallOf<Runtime>, TransactionValidityError> {
-		call.check_obsolete_submit_finality_proof()?;
+		call.check_obsolete_submit_finality_proof(0)?;
 		call.check_obsolete_submit_parachain_heads()?;
 		call.check_obsolete_call()?;
 		Ok(call)
@@ -833,7 +833,7 @@ where
 	fn check_obsolete_parsed_call(
 		call: &CallOf<Runtime>,
 	) -> Result<&CallOf<Runtime>, TransactionValidityError> {
-		call.check_obsolete_submit_finality_proof()?;
+		call.check_obsolete_submit_finality_proof(0)?;
 		call.check_obsolete_call()?;
 		Ok(call)
 	}
