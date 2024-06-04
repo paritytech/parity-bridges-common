0fa499b1e4ce93fde6c07ec75c40034c0457852a
Branislav Kontur <bkontur@gmail.com>
2024-06-04 11:40:58 +0200

    Updated script for regenerate all

--- scripts/regenerate_runtimes.sh
+++ scripts/regenerate_runtimes.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 cd tools/runtime-codegen
-cargo run --bin runtime-codegen -- --from-node-url "wss://rococo-bridge-hub-rpc.polkadot.io:443" > ../../relays/client-bridge-hub-rococo/src/codegen_runtime.rs
-cargo run --bin runtime-codegen -- --from-node-url "wss://rococo-rpc.polkadot.io:443" > ../../relays/client-rococo/src/codegen_runtime.rs
-cargo run --bin runtime-codegen -- --from-node-url "wss://westend-rpc.polkadot.io:443" > ../../relays/client-westend/src/codegen_runtime.rs
-cargo run --bin runtime-codegen -- --from-node-url "wss://kusama-rpc.polkadot.io:443" > ../../relays/client-kusama/src/codegen_runtime.rs
-cargo run --bin runtime-codegen -- --from-node-url "wss://rpc.polkadot.io:443" > ../../relays/client-polkadot/src/codegen_runtime.rs
+cargo run --bin runtime-codegen -- --from-node-url "wss://rococo-bridge-hub-rpc.polkadot.io:443" > ../../relay-clients/client-bridge-hub-rococo/src/codegen_runtime.rs
+cargo run --bin runtime-codegen -- --from-node-url "wss://rococo-rpc.polkadot.io:443" > ../../relay-clients/client-rococo/src/codegen_runtime.rs
+
+cargo run --bin runtime-codegen -- --from-node-url "wss://westend-rpc.polkadot.io:443" > ../../relay-clients/client-westend/src/codegen_runtime.rs
+cargo run --bin runtime-codegen -- --from-node-url "wss://westend-bridge-hub-rpc.polkadot.io:443" > ../../relay-clients/client-bridge-hub-westend/src/codegen_runtime.rs
+
+cargo run --bin runtime-codegen -- --from-node-url "wss://kusama-rpc.polkadot.io" > ../../relay-clients/client-kusama/src/codegen_runtime.rs
+cargo run --bin runtime-codegen -- --from-node-url "wss://kusama-bridge-hub-rpc.polkadot.io" > ../../relay-clients/client-bridge-hub-kusama/src/codegen_runtime.rs
+
+cargo run --bin runtime-codegen -- --from-node-url "wss://dot-rpc.stakeworld.io" > ../../relay-clients/client-polkadot/src/codegen_runtime.rs
+cargo run --bin runtime-codegen -- --from-node-url "wss://polkadot-bridge-hub-rpc.polkadot.io" > ../../relay-clients/client-bridge-hub-polkadot/src/codegen_runtime.rs
 
 # Uncomment to update other runtimes
 
@@ -13,12 +19,16 @@ cargo run --bin runtime-codegen -- --from-node-url "wss://rpc.polkadot.io:443" >
 # TODO: there is a bug, probably needs to update subxt, generates: `::sp_runtime::generic::Header<::core::primitive::u32>` withtout second `Hash` parameter.
 # cargo run --bin runtime-codegen -- --from-wasm-file ../../../polkadot-sdk/target/release/wbuild/bridge-hub-rococo-runtime/bridge_hub_rococo_runtime.compact.compressed.wasm > ../../relays/client-bridge-hub-rococo/src/codegen_runtime.rs
 # cargo run --bin runtime-codegen -- --from-wasm-file ../../../polkadot-sdk/target/release/wbuild/bridge-hub-westend-runtime/bridge_hub_westend_runtime.compact.compressed.wasm > ../../relays/client-bridge-hub-westend/src/codegen_runtime.rs
-# OR for production runtimes:
-# cargo run --bin runtime-codegen -- --from-node-url wss://kusama-bridge-hub-rpc.polkadot.io/ > ../../relay-clients/client-bridge-hub-kusama/src/codegen_runtime.rs
-# cargo run --bin runtime-codegen -- --from-node-url wss://polkadot-bridge-hub-rpc.polkadot.io/ > ../../relay-clients/client-bridge-hub-polkadot/src/codegen_runtime.rs
 
 cd -
-cargo fmt --all
+cargo +nightly fmt --all
+
+# **IMPORTANT**: due to [well-known issue](https://github.com/paritytech/parity-bridges-common/issues/2669)
+find . -name codegen_runtime.rs -exec \
+    sed -i 's/::sp_runtime::generic::Header<::core::primitive::u32>/::sp_runtime::generic::Header<::core::primitive::u32, ::sp_runtime::traits::BlakeTwo256>/g' {} +
+
+cargo +nightly fmt --all
+
 
 # Polkadot Bulletin Chain:
 #
