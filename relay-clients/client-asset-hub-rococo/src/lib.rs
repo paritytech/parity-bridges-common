022226eb1e84e160f22c6988ad6a9b903b7c3bc6
Branislav Kontur <bkontur@gmail.com>
2025-04-04 21:38:01 +0200

    Update relay-clients/client-asset-hub-rococo/src/lib.rs

--- relay-clients/client-asset-hub-rococo/src/lib.rs
+++ relay-clients/client-asset-hub-rococo/src/lib.rs
@@ -124,5 +124,5 @@ impl ChainWithMessages for AssetHubRococo {
 
 impl ChainWithRuntimeVersion for AssetHubRococo {
 	const RUNTIME_VERSION: Option<SimpleRuntimeVersion> =
-		Some(SimpleRuntimeVersion { spec_version: 1_016_001, transaction_version: 6 });
+		Some(SimpleRuntimeVersion { spec_version: 1_017_001, transaction_version: 16 });
 }
