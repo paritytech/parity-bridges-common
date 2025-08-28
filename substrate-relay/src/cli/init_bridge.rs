f5030882d92cf94b60a058a6dbdb97776dfd70da
Branislav Kontur <bkontur@gmail.com>
2025-08-28 13:47:42 +0200

    Do not use sudo

--- substrate-relay/src/cli/init_bridge.rs
+++ substrate-relay/src/cli/init_bridge.rs
@@ -94,14 +94,9 @@ impl BridgeInitializer for PolkadotToPolkadotBulletinCliBridge {
 	fn encode_init_bridge(
 		init_data: <Self::Engine as Engine<Self::Source>>::InitializationData,
 	) -> <Self::Target as Chain>::Call {
-		type RuntimeCall = relay_polkadot_bulletin_client::RuntimeCall;
-		type BridgePolkadotGrandpaCall = relay_polkadot_bulletin_client::BridgePolkadotGrandpaCall;
-		type SudoCall = relay_polkadot_bulletin_client::SudoCall;
-
-		let initialize_call =
-			RuntimeCall::BridgePolkadotGrandpa(BridgePolkadotGrandpaCall::initialize { init_data });
-
-		RuntimeCall::Sudo(SudoCall::sudo { call: Box::new(initialize_call) })
+		relay_polkadot_bulletin_client::RuntimeCall::BridgePolkadotGrandpa(
+			relay_polkadot_bulletin_client::BridgePolkadotGrandpaCall::initialize { init_data },
+		)
 	}
 }
 
