43e20a19f4b8c9b87dce851a97ac592c1a7e2309
Rohit Sarpotdar <rohit.sarpotdar@parity.io>
2025-12-02 21:33:31 +0530

    upgrading Polkadot BH runtime to v2.0.3 release

--- relay-clients/client-bridge-hub-polkadot/src/lib.rs
+++ relay-clients/client-bridge-hub-polkadot/src/lib.rs
@@ -144,5 +144,5 @@ impl ChainWithMessages for BridgeHubPolkadot {
 
 impl ChainWithRuntimeVersion for BridgeHubPolkadot {
 	const RUNTIME_VERSION: Option<SimpleRuntimeVersion> =
-		Some(SimpleRuntimeVersion { spec_version: 2_000_000, transaction_version: 4 });
+		Some(SimpleRuntimeVersion { spec_version: 2_000_003, transaction_version: 4 });
 }
