0d61e1ab10e49d6b6a0e90b62d33dfc91112043f
Rohit Sarpotdar <rohit.sarpotdar@parity.io>
2025-12-28 15:43:30 +0530

    Upgraded BH Kusama to 2.0.4 (#3197)
    * upgraded BH Kusama to 2.0.4
    
    * updating lock file

--- relay-clients/client-bridge-hub-kusama/src/lib.rs
+++ relay-clients/client-bridge-hub-kusama/src/lib.rs
@@ -140,5 +140,5 @@ impl ChainWithMessages for BridgeHubKusama {
 
 impl ChainWithRuntimeVersion for BridgeHubKusama {
 	const RUNTIME_VERSION: Option<SimpleRuntimeVersion> =
-		Some(SimpleRuntimeVersion { spec_version: 1_009_001, transaction_version: 5 });
+		Some(SimpleRuntimeVersion { spec_version: 2_000_004, transaction_version: 5 });
 }
